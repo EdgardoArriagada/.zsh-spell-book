9146f577be0539872bae822ffc383929a55ef101
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2020-10-04 21:10:03 -0300

    clean stopRunningContainers function

--- src/spells/docker/stopRunningContainers.zsh
+++ src/spells/docker/stopRunningContainers.zsh
@@ -1,9 +1,10 @@
 stopRunningContainers() { 
-  local containers=$(docker container ls -q) 
-  if [ ! -z "$containers" ]; then 
+  local runningContainers=$(docker container ls -q)
+
+  if [ ! -z "$runningContainers" ]; then
     echo "${ZSB_INFO} Stopping all running containers..." 
 
-    docker stop $(echo "$containers") && \
+    docker stop $(echo "$runningContainers") && \
       echo "${ZSB_SUCCESS} All containers did halt.\n" && \
       return 0
   fi 
