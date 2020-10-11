5ca7400da2de5063e8dd155a84254c5dd9948bfe
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2020-10-11 00:10:27 -0300

    add a new loading bar

---
+++ src/utils/zsh-spell-book-utils/loadingBar.zsh
@@ -0,0 +1,53 @@
+${zsb}_loadingBar() {
+  local seconds="$1"
+  if [ -z "$seconds" ]; then
+    echo "${ZSB_ERROR} Amount of seconds expected."
+    return 1
+  fi
+
+  local timeFragment=$(($seconds / 20.0))
+
+  echo -ne '[>                    ]   (0%)\r'
+  sleep "$timeFragment"
+  sleep "$timeFragment"
+  echo -ne '[=>                   ]   (5%)\r'
+  sleep "$timeFragment"
+  echo -ne '[==>                  ]   (10%)\r'
+  sleep "$timeFragment"
+  echo -ne '[===>                 ]   (15%)\r'
+  sleep "$timeFragment"
+  echo -ne '[====>                ]   (20%)\r'
+  sleep "$timeFragment"
+  echo -ne '[=====>               ]   (25%)\r'
+  sleep "$timeFragment"
+  echo -ne '[======>              ]   (30%)\r'
+  sleep "$timeFragment"
+  echo -ne '[=======>             ]   (35%)\r'
+  sleep "$timeFragment"
+  echo -ne '[========>            ]   (40%)\r'
+  sleep "$timeFragment"
+  echo -ne '[=========>           ]   (45%)\r'
+  sleep "$timeFragment"
+  echo -ne '[==========>          ]   (50%)\r'
+  sleep "$timeFragment"
+  echo -ne '[===========>         ]   (55%)\r'
+  sleep "$timeFragment"
+  echo -ne '[============>        ]   (60%)\r'
+  sleep "$timeFragment"
+  echo -ne '[=============>       ]   (65%)\r'
+  sleep "$timeFragment"
+  echo -ne '[==============>      ]   (70%)\r'
+  sleep "$timeFragment"
+  echo -ne '[===============>     ]   (75%)\r'
+  sleep "$timeFragment"
+  echo -ne '[================>    ]   (80%)\r'
+  sleep "$timeFragment"
+  echo -ne '[=================>   ]   (85%)\r'
+  sleep "$timeFragment"
+  echo -ne '[==================>  ]   (90%)\r'
+  sleep "$timeFragment"
+  echo -ne '[===================> ]   (95%)\r'
+  sleep "$timeFragment"
+  echo -ne '[====================>]   (100%)\r'
+  echo -ne '\n'
+}
