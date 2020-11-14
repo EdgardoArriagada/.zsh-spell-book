e88359a9ba73459994a15c38bea0c00adefb6771
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2020-11-14 19:12:45 -0300

    add git status to gco command

--- src/spells/git/gco.zsh
+++ src/spells/git/gco.zsh
@@ -1,5 +1,6 @@
+# I only use this for checking out files
 gco() {
-  git checkout "$@"
+  git checkout "$@" && ${zsb}_gitStatus
 }
 
 complete -C "$ZSB_GIT_UNSTAGED_FILES" gco
--- src/spells/git/grh.zsh
+++ src/spells/git/grh.zsh
@@ -1,3 +1,4 @@
+# I only use this to move staged files back to unstaged state
 grh() {
   git reset -q "$@" && ${zsb}_gitStatus
 }
