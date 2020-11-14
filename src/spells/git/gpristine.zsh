683f44191c02550fcd5977862a81ccdfe1c5a638
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2020-11-14 12:42:10 -0300

    add new pristine command

---
+++ src/spells/git/gpristine.zsh
@@ -0,0 +1,7 @@
+gpristine(){
+  echo "${ZSB_WARNING} All your uncommited work will be lost"
+  echo ""
+  echo "${ZSB_PROMPT} Are you sure? [Y/n]"
+  ${zsb}_yesNoMenu && \
+    git reset --hard && git clean -dffx
+}
