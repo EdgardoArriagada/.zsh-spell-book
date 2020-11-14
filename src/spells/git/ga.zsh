1c73ef52e1d0734e5812ceabcde0f85785c85a3d
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2020-11-14 14:42:30 -0300

    improve ga command completion

--- src/spells/git/ga.zsh
+++ src/spells/git/ga.zsh
@@ -2,12 +2,17 @@
 # will trigger 'git add .' instead of 'git add new'
 
 ga() {
-  if [ "$1" = "new" ] || [ "$1" = "." ]; then
+  if [ "$1" = "." ]; then
+    git add . && ${zsb}_gitStatus
+    return 0
+  fi
+
+  if [ "$1" = "new" ]; then
     if [ -z "$2" ]; then
       git add . && ${zsb}_gitStatus
       return 0
     fi
-    shift 1
+    shift 1 # remove 'new' flag
     git add "$@" && ${zsb}_gitStatus
     return 0
   fi
@@ -16,4 +21,17 @@ ga() {
   return 0
 }
 
-complete -C "$ZSB_GIT_UNSTAGED_AND_NEW_FILES" ga
+_${zsb}_ga() {
+  if [ "${COMP_WORDS[1]}" = "." ]; then
+    return 0
+  fi
+
+  if [ "${COMP_WORDS[1]}" = "new" ]; then
+    COMPREPLY=( $(compgen -C "$ZSB_GIT_UNSTAGED_AND_NEW_FILES") )
+    return 0
+  fi
+
+  COMPREPLY=( $(compgen -C "$ZSB_GIT_UNSTAGED_FILES") )
+}
+
+complete -F _${zsb}_ga ga
