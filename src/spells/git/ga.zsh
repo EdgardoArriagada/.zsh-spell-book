52fd68c4761d23d8e7aabacceaa899685e661ae6
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2020-11-29 10:43:05 -0300

    refactor ga to better split responsabilities

--- src/spells/git/ga.zsh
+++ src/spells/git/ga.zsh
@@ -4,39 +4,61 @@ ga() (
   local filesToAdd=( )
 
   ${this}.main() {
-    ${this}.tryToAddFilesWithDotArg && return 0
+    case "$firstArg" in
+      'new')
+        ${this}.addFilesWithNewArg "$@"
+      ;;
+      'fast')
+        ${this}.addFilesWithFastArg "$@"
+      ;;
+      '.')
+        ${this}.addFilesWithDotArg
+      ;;
+      *)
+        ${this}.addFilesDefault
+      ;;
+    esac
+  }
 
-    ${this}.existFlag && shift 1
-
-    if [ "$#" = "0" ]; then
-      ${this}.setFilesToAddFromGitList
+  ${this}.addFilesWithNewArg() {
+    shift 1 # remove 'new' flag
+    if [ "$#"  = "0" ]; then
+      filesToAdd=( $(${zsb}.getGitUntrackedFiles) )
     else
       filesToAdd=( "$@" )
     fi
 
     if ! ${this}.thereAreFilesToAdd; then
-      ${this}.informNoChanges; return 0
+      ${this}.informNoChanges; return $?
     fi
 
-    ${this}.addFilesToGitStaging
+    git add "${filesToAdd[@]}"
   }
 
-  ${this}.tryToAddFilesWithDotArg() {
-    if [ "$firstArg" = "." ]; then
-      git add .
-      return 0
+  ${this}.addFilesWithFastArg() {
+    shift 1 # remove 'fast' flag
+    if [ "$#"  = "0" ]; then
+      filesToAdd=( $(${zsb}.getGitUnstagedFiles) )
+    else
+      filesToAdd=( "$@" )
     fi
-    return 1
+
+    if ! ${this}.thereAreFilesToAdd; then
+      ${this}.informNoChanges; return $?
+    fi
+
+    git add "${filesToAdd[@]}"
   }
 
-  ${this}.existFlag() ([ "$firstArg" = "new" ] || [ "$firstArg" = "fast" ])
+  ${this}.addFilesWithDotArg() {
+    git add .
+    return 0
+  }
 
-  ${this}.setFilesToAddFromGitList() {
-    if [ "$firstArg" = "new" ]; then
-      filesToAdd=( $(${zsb}.getGitUntrackedFiles) )
-      return 0
-    fi
-    filesToAdd=( $(${zsb}.getGitUnstagedFiles) )
+  ${this}.addFilesDefault() {
+    filesToAdd=( "$@" )
+
+    git add -p "${filesToAdd[@]}"
   }
 
   ${this}.thereAreFilesToAdd() [ "${#filesToAdd[@]}" -gt 0 ]
@@ -46,14 +68,6 @@ ga() (
     return 0
   }
 
-  ${this}.addFilesToGitStaging() {
-    if ${this}.existFlag; then
-      git add "${filesToAdd[@]}"
-    else
-      git add -p "${filesToAdd[@]}"
-    fi
-  }
-
   ${this}.main "$@"
   ${zsb}.gitStatus
 )
@@ -61,16 +75,20 @@ ga() (
 _${zsb}.ga() {
   local usedCompletion=( "${COMP_WORDS[@]:1:$COMP_CWORD-1}" )
   local completionList
-
-  if [ "${COMP_WORDS[1]}" = "." ]; then
-    return 0
-  fi
-
-  if [ "${COMP_WORDS[1]}" = "new" ]; then
-    completionList=( $(${zsb}.getGitUntrackedFiles) )
-  else
-    completionList=( $(${zsb}.getGitUnstagedFiles) )
-  fi
+  case "${COMP_WORDS[1]}" in
+    'new')
+      completionList=( $(${zsb}.getGitUntrackedFiles) )
+    ;;
+    'fast')
+      completionList=( $(${zsb}.getGitUnstagedFiles) )
+    ;;
+    '.')
+      return 0
+    ;;
+    *)
+      completionList=( $(${zsb}.getGitUnstagedFiles) )
+    ;;
+  esac
 
   local newCompletion=( $(${zsb}.removeUsedOptions "${usedCompletion[*]}" "${completionList[*]}") )
   COMPREPLY=( $(compgen -W "${newCompletion[*]}") )
