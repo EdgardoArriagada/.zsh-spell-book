b985b80ad873b39cf93af7bc544ecf6f42224b2d
Edgardo Arriagada <edgardo.arriagada.ortiz@gmail.com>
2020-12-05 12:06:31 -0300

    Improve tmux (#10)
    * improve tm command to be used inside tmux session
    
    * improve tmkill command to accept multiple sessions to kill
    
    * improve tmux completion

--- src/spells/tmux/tm.zsh
+++ src/spells/tmux/tm.zsh
@@ -4,9 +4,24 @@ tm() {
 
   [ -z "$targetSession" ] && targetSession="$DEFAULT_SESSION"
 
+  if [ ! -z "$TMUX" ]; then # if inside tmux session
+    if tmux has-session -t "$targetSession" 2>/dev/null; then
+      echo "${ZSB_ERROR} Can't connect to $(hl ${targetSession}) whitin a tmux session."
+      return 1
+    fi
+    tmux new-session -d -s "$targetSession" &&
+      echo "${ZSB_SUCCESS} $(hl ${targetSession}) created."
+    return $?
+  fi
+
   tmux attach-session -t "$targetSession">/dev/null 2>&1 || \
     tmux new -s "$targetSession">/dev/null 2>&1
 }
 
-# If any, complete with a list of current tmux sessions
-complete -C "tmux ls 2>&1 | cut -d':' -s -f1" tm
+_${zsb}.tm() {
+  [ "$COMP_CWORD" -gt "1" ] && return 0
+
+  COMPREPLY=( $(tmls) )
+}
+
+complete -F _${zsb}.tm tm
