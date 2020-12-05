6f77529def46f1bd5ead71ffc5265567ecf250e7
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2020-12-05 11:57:10 -0300

    improve tm command to be used inside tmux session

--- src/spells/tmux/tm.zsh
+++ src/spells/tmux/tm.zsh
@@ -4,6 +4,16 @@ tm() {
 
   [ -z "$targetSession" ] && targetSession="$DEFAULT_SESSION"
 
+  if [ ! -z "$TMUX" ]; then # if inside tmux session
+    if tmux has-session -t "$targetSession" 2>/dev/null; then
+      echo "${ZSB_ERROR} Can't connect to $(hl ${targetSession}) whitin a tmux session."
+      return 1
+    fi
+    tmux new-session -d -s "$targetSession" &&
+      echo "${ZSB_SUCCESS} $(hl ${targetSession}) created."
+    return $?
+  fi
+
   tmux attach-session -t "$targetSession">/dev/null 2>&1 || \
     tmux new -s "$targetSession">/dev/null 2>&1
 }
