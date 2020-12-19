8fd4885b634741f443e3874f36aa647ed08b5356
edgardoarriagada <edgardo.arriagada.ortiz@gmail.com>
2020-12-18 22:39:39 -0300

    change dockerComposeHalt behaviour

--- src/spells/docker/dockerComposeHalt.zsh
+++ src/spells/docker/dockerComposeHalt.zsh
@@ -1,24 +1,14 @@
 ${zsb}.dockerComposeHalt() (
   local this="$0"
   local callback="$1"
-  local activeServices=$(docker-compose ps --services --filter "status=running")
 
   ${this}.main() {
-    if [ -z "$activeServices" ]; then
-      echo "${ZSB_INFO} No services are running for this docker-compose"
-      return 0
-    fi
-
     ${this}.printPrompt
     ${this}.playOptionsMenu
   }
 
   ${this}.printPrompt() {
-    echo "${ZSB_WARNING} The following containers will halt with $(hl ${callback}):"
-    echo " "
-    echo "$(hl "$activeServices")"
-    echo " "
-    echo "${ZSB_PROMPT} Do you really want to stop these containers? [Y/n]"
+    echo "${ZSB_PROMPT} Do you really want to excecute $(hl ${callback})? [Y/n]"
   }
 
   ${this}.playOptionsMenu() {
