cf6868d58558f1bc1d2aa2221212ba9f86ee6255
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2020-12-20 19:07:23 -0300

    add a new galock command that adds lock files

---
+++ src/spells/git/galock.zsh
@@ -0,0 +1,26 @@
+# Automatically add lock files
+galock() (
+  declare -A lockFiles
+  lockFiles[package-lock.json]=true
+  lockFiles[Gemfile.lock]=true
+  lockFiles[yarn.lock]=true
+
+  local redGitFiles=( $(${zsb}.getGitUnstagedAndUntrackedFiles) )
+  local hasAFileBeenAdded=false
+
+  for file in "${redGitFiles[@]}"; do
+    if [ "${lockFiles[$file]}" = "true" ]; then
+      git add "$file"
+      hasAFileBeenAdded=true
+    fi
+  done
+
+  if [ "$hasAFileBeenAdded" = "false" ]; then
+    echo "${ZSB_WARNING} No $(hl "*.lock") files have been added."
+  fi
+
+  ${zsb}.gitStatus
+)
+
+complete galock
+
