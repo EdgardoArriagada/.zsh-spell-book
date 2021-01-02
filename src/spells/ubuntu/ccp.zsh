0249ed4c6a1d1937aef18291d38c82f593411855
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-01-02 18:11:22 -0300

    improve ccp command co include line numbers

--- src/spells/ubuntu/ccp.zsh
+++ src/spells/ubuntu/ccp.zsh
@@ -1,3 +1,16 @@
 ccp() {
-  cat "$1" | clipcopy
+  local inputFile="$1"
+  local leftRange="$2"
+  local rightRange="$3"
+
+  case "$#" in
+    1)
+      cat "$inputFile" | clipcopy ;;
+    2)
+      awk "FNR==$leftRange" $inputFile | clipcopy ;;
+    3)
+      awk "FNR>=$leftRange && FNR<=$rightRange" $inputFile | clipcopy ;;
+    *)
+      echo "${ZSB_ERROR} You must provide a file, an optional line to copy or two numbers describing a range of lines to copy."
+  esac
 }
