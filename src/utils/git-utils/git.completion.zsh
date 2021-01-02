40d740c1f18717ddb261b7521390ce18f2db6053
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-01-02 18:12:17 -0300

    refactor gitFiles to be more maintainable

--- src/utils/git-utils/git.completion.zsh
+++ src/utils/git-utils/git.completion.zsh
@@ -8,18 +8,32 @@ ${zsb}.gitBranches() {
   esac
 }
 
-${zsb}.getGitFiles() {
-  ! ${zsb}.isGitRepo && return 0
-  case "$1" in
-    'staged'|'green')
-      git status --short | grep '^[MARCD]' | sed s/^...// ;;
-    'unstaged')
-      git status --short | grep -E '(^ [MARCD])|(^[MARCD]{2})' | sed s/^...// ;;
-    'untracked')
-      git status --short | grep -E '(^ \?)|(^\?{2})' | sed s/^...// ;;
-    'red')
-      git status --short | grep -E '(^ [MARCD\?])|(^[MARCD\?]{2})' | sed s/^...// ;;
-    *)
-      git status --short | sed s/^...// ;;
-  esac
-}
+${zsb}.getGitFiles() (
+  local this="$0"
+
+  ${this}.main() {
+    ! ${zsb}.isGitRepo && return 0
+    case "$1" in
+      'staged'|'green')
+        ${this}.getgitflesfromregex '^[MARCD]' ;;
+      'unstaged')
+        ${this}.getgitflesfromregex '(^ [MARCD])|(^[MARCD]{2})' ;;
+      'untracked')
+        ${this}.getgitflesfromregex '(^ \?)|(^\?{2})' ;;
+      'red')
+        ${this}.getgitflesfromregex '(^ [MARCD\?])|(^[MARCD\?]{2})' ;;
+      *)
+        ${this}.gitShortStatus | ${this}.removeGitTokens ;;
+    esac
+  }
+
+  ${this}.getgitflesfromregex() {
+    ${this}.gitShortStatus | grep -E "$1" | ${this}.removeGitTokens
+  }
+
+  ${this}.gitShortStatus() git status --porcelain
+
+  ${this}.removeGitTokens() sed 's/^...//'
+
+  ${this}.main "$@"
+)
