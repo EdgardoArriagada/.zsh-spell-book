812a5cbabe5d1bb6575b007456f62c5c8f1d5cb2
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-01-08 22:00:25 -0300

    improve ccp to be able to simply cat a file between ranges

--- src/spells/ubuntu/ccp.zsh
+++ src/spells/ubuntu/ccp.zsh
@@ -5,12 +5,17 @@ ccp() {
 
   case "$#" in
     1)
-      cat "$inputFile" | clipcopy ;;
+      cat "$inputFile" ;;
     2)
-      awk "FNR==$leftRange" $inputFile | clipcopy ;;
+      awk "FNR==$leftRange" $inputFile ;;
     3)
-      awk "FNR>=$leftRange && FNR<=$rightRange" $inputFile | clipcopy ;;
+      awk "FNR>=$leftRange && FNR<=$rightRange" $inputFile ;;
     *)
-      echo "${ZSB_ERROR} You must provide a file, an optional line to copy or two numbers describing a range of lines to copy."
+      echo "${ZSB_ERROR} Povide a file name with: a line number to copy, two numbers as a range or simply nothing."
   esac
 }
+
+cclp() {
+  ccp ${*} | clipcopy
+}
+
