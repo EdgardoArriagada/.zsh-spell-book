aaeb3d99ea5651e5cab6b8ca403debfc33ed9191
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-01-17 21:06:24 -0300

    improve fox command to be able to be used in pipe

--- src/spells/ubuntu/fox.zsh
+++ src/spells/ubuntu/fox.zsh
@@ -1,15 +1,22 @@
 fox() {
-  local inputFile="$1"
-  local leftRange="$2"
-  local rightRange="$3"
+  local inputContent=$(${zsb}.nonPiped "$1")
 
-  case "$#" in
+  # if is a valid filename
+  if [ -f "$inputContent" ]; then
+    inputContent=$(< "$inputContent")
+  fi
+
+  local leftRange=$(${zsb}.nonPiped "$2" "$1")
+  local rightRange=$(${zsb}.nonPiped "$3" "$2")
+  local totalArgs=$(${zsb}.nonPiped "$#" "$(($# + 1))")
+
+  case "$totalArgs" in
     1)
-      < "$inputFile" ;;
+      echo "$inputContent" ;;
     2)
-      awk "FNR==$leftRange" $inputFile ;;
+      echo "$inputContent" | awk "FNR==$leftRange" ;;
     3)
-      awk "FNR>=$leftRange && FNR<=$rightRange" $inputFile ;;
+      echo "$inputContent" | awk "FNR>=$leftRange && FNR<=$rightRange" ;;
     *)
       echo "${ZSB_ERROR} Povide a file name with: a line number to copy, two numbers as a range or simply nothing."
   esac
