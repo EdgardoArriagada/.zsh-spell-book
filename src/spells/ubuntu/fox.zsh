cc0cce85d471356a65796fc3058a505578a281cb
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-01-17 22:02:18 -0300

    improve fox performance

--- src/spells/ubuntu/fox.zsh
+++ src/spells/ubuntu/fox.zsh
@@ -1,26 +1,50 @@
-fox() {
+fox() (
+  local this="$0"
+
   local inputContent=$(${zsb}.nonPiped "$1")
-
-  # if is a valid filename
-  if [ -f "$inputContent" ]; then
-    inputContent=$(< "$inputContent")
-  fi
-
   local leftRange=$(${zsb}.nonPiped "$2" "$1")
   local rightRange=$(${zsb}.nonPiped "$3" "$2")
   local totalArgs=$(${zsb}.nonPiped "$#" "$(($# + 1))")
 
-  case "$totalArgs" in
-    1)
-      echo "$inputContent" ;;
-    2)
-      echo "$inputContent" | awk "FNR==$leftRange" ;;
-    3)
-      echo "$inputContent" | awk "FNR>=$leftRange && FNR<=$rightRange" ;;
-    *)
-      echo "${ZSB_ERROR} Povide a file name with: a line number to copy, two numbers as a range or simply nothing."
-  esac
-}
+  ${this}.main() {
+    case "$totalArgs" in
+      1)
+        ${this}.showInputContent ;;
+      2)
+        ${this}.showCertainLineOfInput ;;
+      3)
+        ${this}.showRangeLinesOfInput ;;
+      *)
+        echo "${ZSB_ERROR} Povide a file name with: a line number to copy, two numbers as a range or simply nothing."
+    esac
+  }
+
+  ${this}.showInputContent() {
+    if [ -f "$inputContent" ]; then
+      cat ${inputContent}
+    else
+      plainPrint "$inputContent"
+    fi
+  }
+
+  ${this}.showCertainLineOfInput() {
+    if [ -f "$inputContent" ]; then
+      awk "FNR==$leftRange" ${inputContent}
+    else
+      plainPrint "$inputContent" | awk "FNR==$leftRange"
+    fi
+  }
+
+  ${this}.showRangeLinesOfInput() {
+    if [ -f "$inputContent" ]; then
+      awk "FNR>=$leftRange && FNR<=$rightRange" ${inputContent}
+    else
+      plainPrint "$inputContent" | awk "FNR>=$leftRange && FNR<=$rightRange"
+    fi
+  }
+
+  ${this}.main "$@"
+)
 
 ccp() {
   fox ${*} | clipcopy
