a78d0a38697678d2acdaa1ff781bb43b7c4bccd6
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-03-27 11:37:57 -0300

    make unz hadle more file formats

--- src/spells/ubuntu/unz.zsh
+++ src/spells/ubuntu/unz.zsh
@@ -5,7 +5,17 @@ unz() (
 
   case "${file}" in
     *.tar.gz) tar -xzf "$file" ;;
+    *.tar.bz2) tar -xjf $file ;;
+    *.tar.xz) tar zxvf "$file";;
+    *.bz2) bunzip2 "$file" ;;
+    *.rar) rar x "$file" ;;
     *.gz) gunzip --keep "$file" ;;
+    *.tar) tar xf "$file";;
+    *.tbz2) ar xjf "$file" ;;
+    *.tgz) tar xzf "$file" ;;
+    *.xz) xz -d "$file" ;;
+    *.zip) unzip "$file" ;;
+    *.Z) uncompress "$file" ;;
     *) ${zsb}.throw 'Unhandled file extension.' ;;
   esac
 )
