789714f90342d71bf8d77db0d54b4bf5a72e8317
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-03-27 00:46:01 -0300

    fix a small typo

--- src/spells/pomodoro/_validateSeconds.zsh
+++ src/spells/pomodoro/_validateSeconds.zsh
@@ -5,8 +5,7 @@ ${zsb}.pomodoro.validateSeconds() {
   [ "$totalSeconds" -lt "$sixtyHours" ] && \
   [ "$totalSeconds" -gt "0" ] && return 0
 
-  ${zsb}.throw "\
-    \r${ZSB_ERROR} Bad argument.
+  ${zsb}.throw "Bad argument.
         \rTry with (hh:)?mm:ss $(it '(min 1, max 59:59:59)')
         OR {s : s ∈ Z and 1 ≤ s ≤ 215999}
         OR ^n[hHmMsS]$ $(it '(min 1s, max 59h)')"
