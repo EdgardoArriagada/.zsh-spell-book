4356771b8de3f1bec775621ddecc5dd08b3fecf1
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-03-26 23:02:08 -0300

    move validation logic to main countdown function

--- src/spells/pomodoro/countdown.zsh
+++ src/spells/pomodoro/countdown.zsh
@@ -1,11 +1,38 @@
-countdown() {
+countdown() (
+  local this="$0"
   local inputTime="$1"
-  totalSeconds=$(${zsb}.convertToSeconds "$inputTime")
+  local totalSeconds
 
-  if ! ${zsb}.didSuccess "$?"; then
-    echo "$totalSeconds" # as error
+  ${this}.main () {
+    totalSeconds="$(${zsb}.convertToSeconds "$inputTime")"
+
+    if ${this}.isTimerOutOfRange; then
+      ${this}.throwBadArgumentException
+    fi
+
+    ${zsb}.runTimerFromSeconds "$totalSeconds"
+  }
+
+
+  ${this}.isTimerOutOfRange() {
+    local sixtyHoursMinusOneSecond="215999"
+
+    [ "$totalSeconds" -gt "$sixtyHoursMinusOneSecond" ] || \
+    [ "$totalSeconds" -lt "1" ] && return 0
     return 1
-  fi
+  }
 
-  ${zsb}.runTimerFromSeconds "$totalSeconds"
-}
+  ${this}.throwBadArgumentException() {
+    ${zsb}.throw "\
+      \r${ZSB_ERROR} Bad argument.
+          \rTry with (hh:)?mm:ss $(it '(min 1, max 59:59:59)')
+          OR {s : s ∈ Z and 1 ≤ s ≤ 215999}
+          OR ^n[hHmMsS]$ $(it '(min 1s, max 59h)')"
+  }
+
+  ${this}.main "$@"
+)
+
+
+
+
