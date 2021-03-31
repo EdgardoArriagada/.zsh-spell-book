0312c269e389fb1637be192bd129fce0f246dd7e
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-03-30 23:02:08 -0300

    add vscode snippets

--- src/setup/vscode.setup.zsh
+++ src/setup/vscode.setup.zsh
@@ -1,3 +1,4 @@
 ${zsb}.safeLink vscode-config/settings.json $HOME/.config/Code/User/settings.json
 ${zsb}.safeLink vscode-config/keybindings.json $HOME/.config/Code/User/keybindings.json
+${zsb}.safeLink vscode-config/snippets $HOME/.config/Code/User/snippets
 
