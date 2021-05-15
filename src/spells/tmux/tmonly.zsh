c8152260096a35c2f634bf81e47cf1fee6eeb28c
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-05-15 12:19:27 -0400

    add a tmonly command that kills other tmux sessions

---
+++ src/spells/tmux/tmonly.zsh
@@ -0,0 +1,15 @@
+tmonly() {
+  local -r tmuxList=( $(tmls) )
+  local -r currentSession=( "$(tmux display-message -p '#S')" )
+  local -r otherSessions=${tmuxList:|currentSession}
+
+  [ -z "$otherSessions" ] && ${zsb}.info "There are no other sessions." && return 0
+
+  ${zsb}.warning "You are about to kill the following sessions"
+  echo "$(hl "$otherSessions")"
+  ${zsb}.prompt "Are you sure? [Y/n]"
+
+  ${zsb}.confirmMenu && tmux kill-session -a
+}
+
+_${zsb}.nocompletion tmonly
