ecbdd1f8980fec3e31cb6a8610a48dc5df74ad62
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-05-14 21:38:54 -0400

    add new nonRepeatedList completion commands

---
+++ src/utils/zsb-utils/completions/nonRepeatedList.zsh
@@ -0,0 +1,32 @@
+# Handles command descriptions
+_${zsb}.nonRepeatedListD() {
+  local usedCompletion=( "${words[@]:1:$CURRENT-2}" )
+  local completionList=( "$@" )
+
+  declare -A tokenDictionary=( )
+  local completionTokens=(  )
+  for item in "${completionList[@]}"; do
+    local token="$(echo ${item} | cut -d':' -f1)"
+    tokenDictionary[$token]="$item"
+    completionTokens+=( "$token" )
+  done
+
+  local newTokens=( ${completionTokens:|usedCompletion} )
+
+  local newCompletion=(  )
+  for token in "${newTokens[@]}"; do
+    newCompletion+=( "${tokenDictionary[$token]}" )
+  done
+
+  _describe 'command' newCompletion
+}
+
+_${zsb}.nonRepeatedList() {
+  local usedCompletion=( "${words[@]:1:$CURRENT-2}" )
+  local completionList=( "$@" )
+
+  local newCompletion=( ${completionList:|usedCompletion} )
+
+  _describe 'command' newCompletion
+}
+
