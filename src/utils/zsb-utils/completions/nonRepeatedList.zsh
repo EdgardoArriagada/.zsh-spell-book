a0e994eecc2c0eefffa4ae42329f5a8981dd6362
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-05-16 11:11:39 -0400

    param expansion needed to be greedier

--- src/utils/zsb-utils/completions/nonRepeatedList.zsh
+++ src/utils/zsb-utils/completions/nonRepeatedList.zsh
@@ -6,7 +6,7 @@ _${zsb}.nonRepeatedListD() {
   declare -A tokenDictionary=( )
   local completionTokens=(  )
   for item in "${completionList[@]}"; do
-    local token="${item%:*}"
+    local token="${item%%:*}"
     tokenDictionary[$token]="$item"
     completionTokens+=( "$token" )
   done
