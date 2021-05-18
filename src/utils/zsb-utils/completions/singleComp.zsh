4a80ac7864bdf7818ee0c92a425ad99736c28978
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-05-17 22:18:30 -0400

    add forgotten file

--- src/utils/zsb-utils/completions/singleComp.zsh
+++ src/utils/zsb-utils/completions/singleComp.zsh
@@ -4,3 +4,24 @@ _${zsb}.singleComp() {
   _describe 'command' comp
 }
 
+_${zsb}.singleCompC() {
+  [[ "$CURRENT" -gt 2 ]] && return 0
+  local -r comp=( $(eval "$1") )
+  _describe 'command' comp
+}
+
+_${zsb}.cachedSingleCompC() {
+  [[ "$CURRENT" -gt 2 ]] && return 0
+  local -r cacheKey=${2:?'You must provide a cacheKey'}
+  local comp
+  local -r isCacheAlive="$(${zsb}.cache.get "$cacheKey")"
+  if [[ "$isCacheAlive" == true ]]; then
+    comp=( "${ZSB_CACHED_COMPLETION[@]}" )
+  else
+    comp=( $(eval "$1") )
+    ZSB_CACHED_COMPLETION=( "${comp[@]}" )
+    ${zsb}.cache.set "$cacheKey" true
+  fi
+  _describe 'command' comp
+}
+
