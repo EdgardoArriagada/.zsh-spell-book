fbffe532b9197abb508f7d7f5286d6bcd3168723
edgardoarriagada <edgardo.arriagada.ortiz@gmail.com>
2021-06-09 19:48:24 -0400

    add leader b r combination

--- src/spells/docker/removeStoppedContainers.zsh
+++ src/spells/docker/removeStoppedContainers.zsh
@@ -2,14 +2,14 @@ removeStoppedContainers() {
   local stoppedContainers=$(docker container ls -aq)
 
   if [[ -z "$stoppedContainers" ]]; then
-    echo "${ZSB_INFO} There are no stopped containers to remove."
+    ${zsb}.info "There are no stopped containers to remove."
     return 0
   fi
 
-  echo "${ZSB_INFO} Removing all stopped Containers..."
+  ${zsb}.info "Removing all stopped Containers..."
 
   docker container rm $(echo "$stoppedContainers") && \
-    echo "${ZSB_SUCCESS} All containers were removed." && \
+    ${zsb}.success "All containers were removed." && \
     return 0
 }
 
