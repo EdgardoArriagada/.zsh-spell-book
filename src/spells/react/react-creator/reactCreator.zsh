6c8ce6f96f33366558e72e083b2b5279918e5a21
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-06-13 15:59:44 -0400

    fix reactCreator

--- src/spells/react/react-creator/reactCreator.zsh
+++ src/spells/react/react-creator/reactCreator.zsh
@@ -1,13 +1,13 @@
 reactCreator() {
-  local TEMPLATES_PATH=${ZSB_DIR}/src/spells/react/react-creator/templates
-  local templateName="$1"
+  local -r TEMPLATES_PATH=${ZSB_DIR}/src/spells/react/react-creator/templates
+  local -r templateName="$1"
 
-  local newComponentPath=${2:?'You must provide a React Component directory path'}
-  local newComponentKebab="$(basename $newComponentPath)"
-  local newComponentUpperCamelCase="$(echo "$newComponentKebab" | sed -r 's/(^|-)([a-z])/\U\2/g')"
+  local -r newComponentPath=${2:?'You must provide a React Component directory path'}
+  local -r newComponentKebab="$(basename $newComponentPath)"
+  local -r newComponentUpperCamelCase="$(echo "$newComponentKebab" | sed -r 's/(^|-)([a-z])/\U\2/g')"
 
-  local inputFlags="$3"
-  local GROUP_FLAGS='o'
+  local -r inputFlags="$3"
+  local -r GROUP_FLAGS='o'
 
   ! ${zsb}.areFlagsInGroup "$inputFlags" "$GROUP_FLAGS" && return 1
 
@@ -15,14 +15,14 @@ reactCreator() {
     ${zsb}.throw "Can't overwrite already existing component, use $(hl "-o") flag to do it anyway"
   fi
 
-  local chosenTemplate=${TEMPLATES_PATH}/${templateName}
+  local -r chosenTemplate=${TEMPLATES_PATH}/${templateName}
 
   # Create components
   mkdir --parents "$newComponentPath" &&
     sed "s/TemplateComponent/${newComponentUpperCamelCase}/g" ${chosenTemplate}/TemplateComponent.tsx \
     > ${newComponentPath}/${newComponentKebab}.tsx &&
     sed "s/TemplateComponent/${newComponentKebab}/g" ${chosenTemplate}/index.tsx \
-    > ${newComponentKebab}/index.tsx
+    > ${newComponentPath}/index.tsx
 
 
   if [[ "$?" == "0" ]]; then
