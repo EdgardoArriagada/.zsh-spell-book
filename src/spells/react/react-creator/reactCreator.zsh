9051e86c3ff21f94d215094414e494d12500d5ac
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-06-13 15:48:26 -0400

    make react creator kebab name

--- src/spells/react/react-creator/reactCreator.zsh
+++ src/spells/react/react-creator/reactCreator.zsh
@@ -2,41 +2,34 @@ reactCreator() {
   local TEMPLATES_PATH=${ZSB_DIR}/src/spells/react/react-creator/templates
   local templateName="$1"
 
-  local newComponentPath="$2"
-  local newComponentName="$(basename $newComponentPath)"
+  local newComponentPath=${2:?'You must provide a React Component directory path'}
+  local newComponentKebab="$(basename $newComponentPath)"
+  local newComponentUpperCamelCase="$(echo "$newComponentKebab" | sed -r 's/(^|-)([a-z])/\U\2/g')"
 
   local inputFlags="$3"
   local GROUP_FLAGS='o'
 
   ! ${zsb}.areFlagsInGroup "$inputFlags" "$GROUP_FLAGS" && return 1
 
-  if [[ -z "$newComponentPath" ]]; then
-    echo "${ZSB_ERROR} You must provide a React Component directory path"
-    return 1
-  fi
-
-
   if [[ -d "$newComponentPath" ]] && [[ "$inputFlags" != *'o'* ]] ; then
-    echo "${ZSB_ERROR} Can't overwrite already existing component, use $(hl "-o") flag to do it anyway"
-    return 1
+    ${zsb}.throw "Can't overwrite already existing component, use $(hl "-o") flag to do it anyway"
   fi
 
-
-  local replaceRegex="s/TemplateComponent/${newComponentName}/g"
   local chosenTemplate=${TEMPLATES_PATH}/${templateName}
 
   # Create components
   mkdir --parents "$newComponentPath" &&
-    sed "$replaceRegex" ${chosenTemplate}/TemplateComponent.tsx \
-    > ${newComponentPath}/${newComponentName}.tsx &&
-    sed "$replaceRegex" ${chosenTemplate}/index.tsx \
-    > ${newComponentPath}/index.tsx
+    sed "s/TemplateComponent/${newComponentUpperCamelCase}/g" ${chosenTemplate}/TemplateComponent.tsx \
+    > ${newComponentPath}/${newComponentKebab}.tsx &&
+    sed "s/TemplateComponent/${newComponentKebab}/g" ${chosenTemplate}/index.tsx \
+    > ${newComponentKebab}/index.tsx
 
 
   if [[ "$?" == "0" ]]; then
-    echo "${ZSB_SUCCESS} New component -> ${newComponentPath}/$(hl "${newComponentName}.tsx")"
+    echo "${ZSB_SUCCESS} New component -> ${newComponentPath}/$(hl "${newComponentKebab}.tsx")"
   fi
 }
 
 alias createreactcomponent="reactCreator FC"
 alias createnextpage="reactCreator NEXTPAGE"
+
