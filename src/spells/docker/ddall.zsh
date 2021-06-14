6f0213592cc0362ca9a9a1c8c3cdf3016351acb3
edgardoarriagada <edgardo.arriagada.ortiz@gmail.com>
2021-06-14 15:46:41 -0400

    clean ddall

--- src/spells/docker/ddall.zsh
+++ src/spells/docker/ddall.zsh
@@ -1,8 +1,8 @@
 # like `docker-compose down` but for all the containers
 ddall() {
-  stopRunningContainers; \
-    removeStoppedContainers; \
-    docker network prune --force
+  stopRunningContainers
+  removeStoppedContainers
+  docker network prune --force
 }
 
 _${zsb}.nocompletion ddall
