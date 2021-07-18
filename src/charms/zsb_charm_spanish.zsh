14608939c113ff8a22ccbd9c2449cec67a827d8b
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-07-17 22:19:43 -0400

    add a new spanish characters charm

---
+++ src/charms/zsb_charm_spanish.zsh
@@ -0,0 +1,29 @@
+#!/usr/bin/env zsh
+set -o errexit
+set -o nounset
+set -o pipefail
+
+declare -Ar SPANISH_CHARACTERS=(
+  [a\ =\>\ á]='á'
+  [e\ =\>\ é]='é'
+  [i\ =\>\ í]='í'
+  [o\ =\>\ ó]='ó'
+  [u\ =\>\ ú]='ú'
+  [n\ =\>\ ñ]='ñ'
+
+  [A\ =\>\ Á]='Á'
+  [E\ =\>\ É]='É'
+  [I\ =\>\ Í]='Í'
+  [O\ =\>\ Ó]='Ó'
+  [U\ =\>\ Ú]='Ú'
+  [N\ =\>\ Ñ]='Ñ'
+)
+
+declare -r selection=$(print -l "${(@k)SPANISH_CHARACTERS}" | rofi -show -width -8 -dmenu -auto-select -no-custom -p "")
+
+[[ -z "$selection" ]] && exit 0
+
+echo "${SPANISH_CHARACTERS[$selection]}" | xclip -r -selection clipboard && xdotool key shift+Insert
+
+exit 0
+
