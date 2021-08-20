f701207d20435de85417f75292403c21e1df1219
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-08-20 19:05:55 -0400

    improve dailyCode format

--- src/spells/node/dailycode/template/code.test.js
+++ src/spells/node/dailycode/template/code.test.js
@@ -1,10 +1,13 @@
-function solverFunction (array) {
-    return array
+function getResult(input) {
+  return Object.entries(input).map(([k, v]) => ({[k]: v}))
 }
+
 describe('daily code problem', () => {
-    it('should succesfully test', () => {
-	const input = [0, 1, 2]
-	const expectedResult = [0, 1, 2]    
-        expect(solverFunction(input)).toEqual(expectedResult)
-    })
+  it('should succesfully test', () => {
+    const input = {dos: 2, uno: 1, tres: 3}
+
+    const result = getResult(input)
+
+    expect(result).toEqual([{dos: 2}, {uno: 1}, {tres: 3}])
+  })
 })
