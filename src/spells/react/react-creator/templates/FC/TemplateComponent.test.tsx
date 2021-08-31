5bdfd196a738ca98a81ec66c7806c87537f1654d
edgardoarriagada <edgardo.arriagada.ortiz@gmail.com>
2021-08-31 09:52:30 -0400

    update template component

--- src/spells/react/react-creator/templates/FC/TemplateComponent.test.tsx
+++ src/spells/react/react-creator/templates/FC/TemplateComponent.test.tsx
@@ -1,9 +1,13 @@
 import TemplateComponent from './template-component'
 import { render } from 'setupTests'
 
+const setUp = () => {
+  return render(<TemplateComponent/>)
+}
+
 describe('TemplateComponent component', () => {
   it('should render successfully', () => {
-    const c = render(<TemplateComponent/>)
+    const c = setUp()
     expect(c.container).toBeInTheDocument()
   })
 })
