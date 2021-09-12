59cb1a0370a485d0b245728ee0afbb2d73531614
edgardoarriagada <edgardo.arriagada.ortiz@gmail.com>
2021-09-11 21:23:05 -0300

    add notebook spell

--- src/spells/notebook/notebook.zsh
+++ src/spells/notebook/notebook.zsh
@@ -1,12 +1,24 @@
-notebook() {
+${zsb}.notebook.validateEnv() {
   ${zsb}.validate "ZSB_NOTEBOOK_DIR"
   ${zsb}.validate "ZSB_NOTEBOOK_CHAPTER"
   ${zsb}.validate "ZSB_NOTEBOOK_PAGE"
+}
+
+${zsb}.notebook() {
+  local -r callback=${1:?'You must provide a callback'}
+
+  ${zsb}.notebook.validateEnv
 
   local -r currentPage="${ZSB_NOTEBOOK_DIR}/${ZSB_NOTEBOOK_CHAPTER}/${ZSB_NOTEBOOK_PAGE}"
 
   # Create parent folder
   mkdir -p $(dirname $currentPage)
 
-  eval "nnvim ${currentPage}"
+  eval "${callback} ${currentPage}"
 }
+
+alias cdpage="${zsb}.notebook.validateEnv && cds ${ZSB_NOTEBOOK_DIR}/${ZSB_NOTEBOOK_CHAPTER}"
+alias page="${zsb}.notebook nnvim"
+alias cpage="${zsb}.notebook batcat"
+alias ccppage="${zsb}.notebook ccp"
+
