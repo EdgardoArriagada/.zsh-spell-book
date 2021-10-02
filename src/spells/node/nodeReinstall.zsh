8da2a128c1c6a77c4c83f6709298fc5d82d544e8
edgardoarriagada <edgardo.arriagada.ortiz@gmail.com>
2021-10-02 10:26:55 -0300

    refactor nodeReinstall with zparseopts

--- src/spells/node/nodeReinstall.zsh
+++ src/spells/node/nodeReinstall.zsh
@@ -1,25 +1,19 @@
-nodeReinstall() {
+${zsb}.nodeReinstall() {
   local packageManager=$1
   local packageManagerLock=$2
 
-  {
-    local inputFlags=$3
-    local GROUP_FLAGS='lni'
+  zparseopts -D -E -F -- l=keepLockFile n=keepNodeModules i=skipInstall || return 1
 
-    ! ${zsb}.areFlagsInGroup "$inputFlags" "$GROUP_FLAGS" && return 1
-  }
-
-  if [[ ! "$inputFlags" == *"l"* ]] && [[ -f ${packageManagerLock} ]]; then
-    rm ${packageManagerLock} && echo "${ZSB_INFO} $(hl ${packageManagerLock}) deleted"
+  if [[ -z "$keepLockFile" ]] && [[ -f ${packageManagerLock} ]]; then
+    rm ${packageManagerLock} && ${zsb}.info "$(hl ${packageManagerLock}) deleted"
   fi
 
-  if [[ ! "$inputFlags" == *"n"* ]] && [[ -d node_modules ]]; then
-    rm -rf node_modules && echo "${ZSB_INFO} $(hl 'node_modules') deleted"
+  if [[ -z "$keepNodeModules" ]] && [[ -d node_modules ]]; then
+    rm -rf node_modules && ${zsb}.info "$(hl 'node_modules') deleted"
   fi
 
-  if [[ "$inputFlags" == *"i"* ]]; then
-    echo "${ZSB_INFO} installation cancelled"
-    return 0
+  if [[ -n "$skipInstall" ]]; then
+    ${zsb}.info "Installation cancelled"; return 0
   fi
 
   # BUG: npm always tries to acess to zsb_lazy_load inside a bash function
@@ -27,8 +21,8 @@ nodeReinstall() {
   eval "${packageManager} install" && ${zsb}.isGitRepo && ${zsb}.gitStatus
 }
 
-complete -W "-l -n -i -lni" nodeReinstall
+compdef "_${zsb}.nonRepeatedListD '-l:Keep lock files' '-n:Keep node modules' '-i:Skip installation'" ${zsb}.nodeReinstall
 
-alias npmreinstall="nodeReinstall npm package-lock.json"
+alias npmreinstall="${zsb}.nodeReinstall npm package-lock.json"
 
-alias yarnreinstall="nodeReinstall yarn yarn.lock"
+alias yarnreinstall="${zsb}.nodeReinstall yarn yarn.lock"
