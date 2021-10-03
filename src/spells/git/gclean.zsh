1783b61de5cdb61fd903deaee33fd640150f06c0
edgardoarriagada <edgardo.arriagada.ortiz@gmail.com>
2021-10-03 13:18:58 -0300

    improve gclean

--- src/spells/git/gclean.zsh
+++ src/spells/git/gclean.zsh
@@ -3,8 +3,18 @@ gclean() {
     git clean -fd "$@"; return $?
   fi
 
-  ${zsb}.warning "All the added files and folders will be deleted."
-  echo ""
+  local untrackedFiles=( $(${zsb}.getGitFiles 'untracked') )
+
+  if [[ -z "$untrackedFiles" ]]; then
+    ${zsb}.info "There are no untracked files."; return 0
+  fi
+
+  local formattedFiles=$(print -rl -- "  ${(z)^untrackedFiles}")
+
+  ${zsb}.warning "All untracked files will be deleted."
+  echo " "
+  echo "$(hl $formattedFiles)"
+  echo " "
   ${zsb}.prompt "Are you sure? [Y/n]"
 
   ${zsb}.confirmMenu && git clean -fd
