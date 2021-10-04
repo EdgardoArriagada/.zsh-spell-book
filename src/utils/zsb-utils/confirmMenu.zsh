a25dcfb5c4e6acd3698eb063e09f17dd4ff575ba
edgardoarriagada <edgardo.arriagada.ortiz@gmail.com>
2021-10-03 23:22:23 -0300

    boy scout

--- src/utils/zsb-utils/confirmMenu.zsh
+++ src/utils/zsb-utils/confirmMenu.zsh
@@ -1,15 +1,11 @@
 ${zsb}.confirmMenu() {
   while true; do
     read yn
+
     case $yn in
-    [Yy]*)
-      return 0 ;;
-    [Nn]*)
-      echo "${ZSB_INFO} Cancelled."
-      return 1
-      ;;
-    *)
-      echo "${ZSB_PROMPT} Please answer yes or no" ;;
+      [Yy]*) return 0 ;;
+      [Nn]*) ${zsb}.info "Cancelled."; return 1 ;;
+      *) ${zsb}.prompt "Please answer yes or no" ;;
     esac
   done
 }
