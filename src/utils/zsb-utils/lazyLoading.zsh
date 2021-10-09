fcf58c971eb19ee296c12ab6a5f26c03d70fd1e6
edgardoarriagada <edgardo.arriagada.ortiz@gmail.com>
2021-10-09 14:14:28 -0300

    trim lazy_load script

--- src/utils/zsb-utils/lazyLoading.zsh
+++ src/utils/zsb-utils/lazyLoading.zsh
@@ -1,41 +1,17 @@
 # usefull link https://gist.github.com/QinMing/364774610afc0e06cc223b467abe83c0#file-zshrc-L15-L37
 # thanks to QinMing for the code!
 ${zsb}.lazy_load() {
-  local this="$0_$(${zsb}.timeId)"
-  local -a involvedScripts
   local aliasesToRelease=("${(@s: :)${1}}") # space separated list
   local scriptToLazyLoad="$2"
   local commandToRun="$3"
   shift 3
   local commandToRunArgs=$*
 
-  {
-    ${this}.main() {
-      ${zsb}.info "Lazy loading..."
+  ${zsb}.info "Lazy loading..."
 
-      ${this}.unaliasInvolvedScripts "$@"
-
-      ${this}.sourceScriptToLazyLoad
-
-      ${this}.runOriginalCommandWithArgs
-    }
-
-    ${this}.unaliasInvolvedScripts() {
-      unalias "${aliasesToRelease[@]}"
-    }
-
-    ${this}.sourceScriptToLazyLoad() {
-      source "$scriptToLazyLoad"
-    }
-
-    ${this}.runOriginalCommandWithArgs() {
-      eval "${commandToRun} ${commandToRunArgs}"
-    }
-
-    ${this}.main "$@"
-  } always {
-    unfunction -m "${this}.*"
-  }
+  unalias "${aliasesToRelease[@]}"
+  source "$scriptToLazyLoad"
+  eval "${commandToRun} ${commandToRunArgs}"
 }
 
 __${zsb}.prepare_lazy_load() {
