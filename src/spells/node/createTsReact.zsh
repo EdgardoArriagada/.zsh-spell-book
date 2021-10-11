5142b38c251740c1197ff000dab5f14659a9373a
edgardoarriagada <edgardo.arriagada.ortiz@gmail.com>
2021-10-11 16:03:28 -0300

    improve createTsReact

--- src/spells/node/createTsReact.zsh
+++ src/spells/node/createTsReact.zsh
@@ -1,5 +1,6 @@
 createTsReact() {
   npx create-react-app "$1" --template typescript && \
+    yarn add -D prettier && \
     prettierInit && \
     alert -i 'face-cool' "Happy Hacking" && \
     return 0
