889d19df1635c1f68c1262500e11dfe1337af14e
edgardoarriagada <edgardo.arriagada.ortiz@gmail.com>
2021-10-11 15:55:28 -0300

    improve alert to be able to receive icons

--- src/utils/alert.zsh
+++ src/utils/alert.zsh
@@ -1,4 +1,11 @@
-alert() notify-send --icon timer --urgency critical "$*"
+alert() {
+  zparseopts -D -E -F -- i:=_icon || return 1
+  local icon="${_icon[2]}"
+  : ${icon:='timer'}
+
+  notify-send --icon "$icon" --urgency critical "$*"
+}
+
 alias a="alert"
 alias ad="alert 'DONE'"
 
