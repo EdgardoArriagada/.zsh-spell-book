4f4a1507ca67627b752dbb6d1767269f13ec4cd8
edgardoarriagada <edgardo.arriagada.ortiz@gmail.com>
2021-10-16 22:26:18 -0300

    make kebab-case cover a special case

--- src/__tests__/utils/kebab-case.test.zsh
+++ src/__tests__/utils/kebab-case.test.zsh
@@ -4,6 +4,8 @@ describe "kebab-case function"; () (
     ['pascalCase']="pascal-case"
     ['kebab-case']="kebab-case"
     ['snake_case']="snake-case"
+    ['_bad_snake_case']="bad-snake-case"
+    ['.dot-project']=".dot-project"
     ['Capital Letters']="capital-letters"
     ['UpperCamelCase']="upper-camel-case"
     ['multiple  spaces   separated ']="multiple-spaces-separated"
--- src/utils/kebab-case.zsh
+++ src/utils/kebab-case.zsh
@@ -1,10 +1,9 @@
 kebab-case() {
   local result=$(echo "$1" |
     sed 's/[A-Z]/ &/g' | # Add spaces before every capital letter
-    sed 's/^ //' | # Remove space at the beginning
     sed 's/[ _]/-/g' | # Replace spaces and underscores by dashes
     tr -s '-' | # Remove duplicated dashes
-    sed 's/-$//') # remove final dash if any
+    sed 's/^-//;s/-$//') # Remove dashes at the beginning and at the end
 
   echo "${(L)result}" # Lowercase the result
 }
