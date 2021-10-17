b3802efbeb63178deaa93fdec7ea616d453b7e12
edgardoarriagada <edgardo.arriagada.ortiz@gmail.com>
2021-10-16 22:02:55 -0300

    fix a typo

--- src/utils/kebab-case.zsh
+++ src/utils/kebab-case.zsh
@@ -1,7 +1,7 @@
 kebab-case() {
   local result=$(echo "$1" |
     sed 's/[A-Z]/ &/g' | # Add spaces before every capital letter
-    sed 's/^ //' | # Remove space at the beggining
+    sed 's/^ //' | # Remove space at the beginning
     sed 's/[ _]/-/g' | # Replace spaces and underscores by dashes
     tr -s '-' | # Remove duplicated dashes
     sed 's/-$//') # remove final dash if any
