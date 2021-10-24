4a292d508851bc4b5766501d7a2f0d2a679bf85d
edgardoarriagada <edgardo.arriagada.ortiz@gmail.com>
2021-10-24 17:29:33 -0300

    inform the user that the new wallpaper was set

--- src/spells/ubuntu/setWallpaper.zsh
+++ src/spells/ubuntu/setWallpaper.zsh
@@ -2,10 +2,11 @@ setWallpaper() {
   [[ ! -d ~/Wallpapers ]] && mkdir ~/Wallpapers
   [[ -z "$(ls ~/Wallpapers)" ]] && ${zsb}.throw "You don't have any wallpapers in $(hl ~/Wallpapers)."
 
-  local chosenWallpaper=$(builtin cd ~/Wallpapers && ls | sxiv -ftio | awk 'FNR == 1')
+  local chosenWallpaper=$(builtin cd ~/Wallpapers && ls | sxiv -ftio | head -1)
 
   [[ -z "$chosenWallpaper" ]] && ${zsb}.info "Cancelled." && return 1
-  xwallpaper --center ~/Wallpapers/${chosenWallpaper}
+  xwallpaper --center ~/Wallpapers/${chosenWallpaper} &&
+    ${zsb}.success "New wallpaper is $(hl ${chosenWallpaper})"
 
   # Persistence after reboot (see .xprofile)
   find ~/temp/wallpaper -lname "${HOME}/Wallpapers/*" -delete
