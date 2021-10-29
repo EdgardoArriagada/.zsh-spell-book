bf183cb1889a316d7e98fb01cf17f0782c3c3406
edgardoarriagada <edgardo.arriagada.ortiz@gmail.com>
2021-10-29 14:50:33 -0300

    small boy scout

--- src/spells/ubuntu/setWallpaper.zsh
+++ src/spells/ubuntu/setWallpaper.zsh
@@ -5,11 +5,12 @@ setWallpaper() {
   local chosenWallpaper=$(builtin cd ~/Wallpapers && ls | sxiv -ftio | head -1)
 
   [[ -z "$chosenWallpaper" ]] && ${zsb}.info "Cancelled." && return 1
+
   xwallpaper --center ~/Wallpapers/${chosenWallpaper} &&
     ${zsb}.success "New wallpaper is $(hl ${chosenWallpaper})"
 
   # Persistence after reboot (see .xprofile)
-  find ~/temp/wallpaper -lname "${HOME}/Wallpapers/*" -delete
   mkdir -pv ~/temp/wallpaper
+  find ~/temp/wallpaper -lname "${HOME}/Wallpapers/*" -delete
   ln -s ~/Wallpapers/${chosenWallpaper} ~/temp/wallpaper/
 }
