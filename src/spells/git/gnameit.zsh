ae2b83a1aecd7691852670073c8318bb26465298
edgardoarriagada <edgardo.arriagada.ortiz@gmail.com>
2021-10-30 18:28:49 -0300

    boy scout

--- src/spells/git/gnameit.zsh
+++ src/spells/git/gnameit.zsh
@@ -3,25 +3,21 @@ gnameit() {
   local userName="${$(git config user.name):l}"
   local oldUrl="$(git config --get remote.origin.url)"
 
-  if [[ -z "$userName" ]]; then
+  [[ -z "$userName" ]] &&
     ${zsb}.throw "Username not set."
-  fi
 
-  if [[ -z "$oldUrl" ]]; then
+  [[ -z "$oldUrl" ]] &&
     ${zsb}.throw "Origin url not set."
-  fi
 
-  if ${zsb}.doesMatch "$oldUrl" "^http[s]:\/{2}.+@"; then
-    printf "${ZSB_INFO} Username in url is already set.
-      $(hl $oldUrl) \n"
-    return 0
-  fi
+  ${zsb}.doesMatch "$oldUrl" "^http[s]:\/{2}.+@" &&
+    ${zsb}.cancel "Username in url is already set.
+    \r$(hl $oldUrl)"
 
   local regexReplacer="s/:\/{2}/:\/\/${userName}@/"
   local newUrl="$(printf "$oldUrl" | sed -E "$regexReplacer")"
 
   git remote set-url origin "$newUrl" && \
-    printf "${ZSB_SUCCESS} Remote url changed from
-      ${ZSB_COLOR_FOREGROUND_RED} - ${oldUrl}${ZSB_NO_COLOR}
-      ${ZSB_COLOR_FOREGROUND_GREEN} + ${newUrl}${ZSB_NO_COLOR} \n"
+    ${zsb}.success "Url changed from
+      \r${ZSB_COLOR_FOREGROUND_RED} - ${oldUrl}${ZSB_NO_COLOR}
+      \r${ZSB_COLOR_FOREGROUND_GREEN} + ${newUrl}${ZSB_NO_COLOR}"
 }
