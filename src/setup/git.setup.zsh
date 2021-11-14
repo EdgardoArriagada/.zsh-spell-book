6b0e99a87e5a74f9ac981c5f129b69ceaf02e544
edgardoarriagada <edgardo.arriagada.ortiz@gmail.com>
2021-11-14 15:12:25 -0300

    sort git setup a little bit

--- src/setup/git.setup.zsh
+++ src/setup/git.setup.zsh
@@ -5,12 +5,15 @@ if git --version >/dev/null 2>&1; then
   git config --global --replace-all core.excludesfile ${ZSB_DIR}/src/dotFiles/git/global.gitignore
 
   git config --global pager.branch false # print branch info instead of "less" it
-  git config --global pager.diff delta
-  git config --global pager.log delta
-  git config --global pager.reflog delta
-  git config --global pager.show delta
-  git config --global include.path ${ZSB_DIR}/src/dotFiles/git/themes.gitconfig
+  git config --global pager.diff 'delta | less'
+  git config --global pager.log 'delta | less'
+  git config --global pager.reflog 'delta | less'
+  git config --global pager.show 'delta | less'
+
   git config --global interactive.diffFilter 'delta --color-only'
+
+  # theme
+  git config --global include.path ${ZSB_DIR}/src/dotFiles/git/themes.gitconfig
   git config --global delta.features calochortus-lyallii
 fi
 
