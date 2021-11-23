2a8bba670233e8ae193f488b1251ff6b29081f14
edgardoarriagada <edgardo.arriagada.ortiz@gmail.com>
2021-11-23 15:11:08 -0300

    add usefull aliases to search through env files

--- src/spells/files/files.aliases.zsh
+++ src/spells/files/files.aliases.zsh
@@ -1,5 +1,7 @@
-alias c="batcat --paging=never"
-alias le="batcat --paging=always"
+alias c='batcat --paging=never'
+alias le='batcat --paging=always'
 alias ls='lsd'
-alias removeSpanishChars="iconv -f utf8 -t ascii//TRANSLIT"
+alias removeSpanishChars='iconv -f utf8 -t ascii//TRANSLIT'
+alias catenv='ls -a | rg env | xargs cat'
+alias batcatenv='ls -a | rg env | xargs batcat'
 
