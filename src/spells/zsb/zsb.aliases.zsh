743b4005f512719f9c1a2a0cac0c4d01909a6081
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-12-24 13:03:53 -0300

    make zshenv location configurable

--- src/spells/zsb/zsb.aliases.zsh
+++ src/spells/zsb/zsb.aliases.zsh
@@ -2,5 +2,5 @@ alias cdzsb="cds ${ZSB_DIR}"
 alias vzsb="cdzsb && nnvim"
 alias galias="alias | rg"
 alias his="nnvim ~/.zsh_history"
-alias env="nnvim ~/temp/.zshenv"
+alias env="nnvim ${ZSB_ZSHENV}"
 
