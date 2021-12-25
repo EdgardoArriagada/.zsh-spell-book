93c4d82f1fa2c79490bf61e2d4f91ccfc7368987
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-12-25 13:35:30 -0300

    save skdh file

--- src/setup/sxhkd.setup.zsh
+++ src/setup/sxhkd.setup.zsh
@@ -1,3 +1,7 @@
-${zsb}.safeLink sxhkd/sxhkd.desktop ~/.config/autostart/sxhkd.desktop
+if (( $ZSB_MACOS )); then
+  ${zsb}.safeLink skhd/.skhdrc ~/.skhdrc
+else
+  ${zsb}.safeLink sxhkd/sxhkd.desktop ~/.config/autostart/sxhkd.desktop
 
-${zsb}.safeLink sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+  ${zsb}.safeLink sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+fi
