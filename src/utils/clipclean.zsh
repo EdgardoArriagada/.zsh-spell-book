55d4f6fd9dd98dfbe5bfa22209b718e551054cc9
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-12-28 09:27:46 -0300

    make clipclean compatible with macos

--- src/utils/clipclean.zsh
+++ src/utils/clipclean.zsh
@@ -1,5 +1,10 @@
 clipclean() {
-  print " " |
-    xclip -selection clipboard &&
-    notify-send --urgency "low" --icon security-high "The clipboard has been cleaned"
+  local NOTIFICATION="The clipboard has been cleaned"
+
+  if (( $ZSB_MACOS )); then
+    print " " | pbcopy && say -v Karen "$NOTIFICATION"
+  else
+    print " " | xclip -selection clipboard &&
+      notify-send --urgency "low" --icon security-high "$NOTIFICATION"
+  fi
 }
