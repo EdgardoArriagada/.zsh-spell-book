d45da4454a4f7a7fdd6ef1abed492d45303976ff
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-12-28 23:19:57 -0300

    improve cdr by adding completion

--- src/spells/ubuntu/cdr.zsh
+++ src/spells/ubuntu/cdr.zsh
@@ -1,5 +1,18 @@
 cdr() {
+  if [[ -n "$1" ]]; then
+    eval "cd ${1}"
+    return $?
+  fi
+
   local choosenDir=$(dirs -p | fzf)
   [[ -z "$choosenDir" ]] && return 0
   eval "cd ${choosenDir}"
 }
+
+_${zsb}.cdr() {
+  local newCompletion=( $(dirs -p) )
+
+  _${zsb}.verticalComp "newCompletion"
+}
+
+compdef _${zsb}.cdr cdr
