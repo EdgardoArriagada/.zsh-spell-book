8042588785b8374977734ab671b89a8f0a0b740d
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2021-12-28 22:50:37 -0300

    improve vg, cg and cdg with fzf

--- src/spells/files/searchFileAndOpen.zsh
+++ src/spells/files/searchFileAndOpen.zsh
@@ -1,30 +1,8 @@
-searchFileAndOpen() {
-  local programToOpenFile="$1"
-  local fileToSearch="$2"
-
-  if [[ -z "$fileToSearch" ]]; then
-    echo "${ZSB_ERROR} You must specify a file name"
-    return 1
-  fi
-
-  local sanitizedInput=$(echo "$fileToSearch" | rev | cut -d"/" -f1 | rev)
-  local parentDir="$(dirname $fileToSearch)"
-
-  local FOUND_FILE=$(fastFind "$parentDir" "$sanitizedInput" "f")
-
-  if [[ ! -f "$FOUND_FILE" ]]; then
-    local tailMessage
-    if [[ "$parentDir" != "." ]]; then tailMessage="in $(hl "$parentDir") directory"; fi
-
-    echo "${ZSB_INFO} the file $(hl "$sanitizedInput") was not found ${tailMessage}"
-    return 1
-  fi
-
-  echo "${ZSB_INFO} File found: $(hl "$FOUND_FILE")"
-
-  eval "${programToOpenFile} ${FOUND_FILE}"
+${zsb}.searchFileAndOpen() {
+  local choosenDir=$(fd -t f | fzf)
+  [[ -z "$choosenDir" ]] && return 0
+  eval "${1} ${choosenDir}"
 }
 
-alias catg="searchFileAndOpen cat"
-alias vimg="searchFileAndOpen vim"
-alias vg="vimg"
+alias cg="${zsb}.searchFileAndOpen c"
+alias vg="${zsb}.searchFileAndOpen v"
--- src/spells/ubuntu/cdg.zsh
+++ src/spells/ubuntu/cdg.zsh
@@ -1,18 +1,5 @@
 cdg() {
-  local DIR="$1"
-  if [[ -z "$DIR" ]]; then
-    echo "${ZSB_ERROR} You must specify a dir name"
-    return 1
-  fi
-
-  local FOUND_DIR=$(fastFind $(pwd) "$DIR" d)
-
-  if [[ ! -d "$FOUND_DIR" ]]; then
-    echo "${ZSB_INFO} the dir $(hl "$DIR") was not found"
-    return 1
-  fi
-
-  echo "${ZSB_INFO} Dir found: $(hl "$FOUND_DIR")"
-  printf %"$COLUMNS"s | tr " " "_"
-  cd "$FOUND_DIR"
+  local choosenDir=$(fd -t d | fzf)
+  [[ -z "$choosenDir" ]] && return 0
+  eval "cd ${choosenDir}"
 }
