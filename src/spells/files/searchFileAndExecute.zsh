c6af82454c1d062d88a720a320001fa35cd5ae02
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-01-03 21:46:59 -0300

    add include hidden ounterparts

--- src/spells/files/searchFileAndExecute.zsh
+++ src/spells/files/searchFileAndExecute.zsh
@@ -26,15 +26,30 @@ vg_command='v'
 vg() ${zsb}.searchFileAndExecute "$0" "$vg_command" "$vg_dataGetter" "$@"
 compdef "_${zsb}.searchFileAndExecute '${vg_dataGetter}'" vg
 
+vgh_dataGetter='fd -t f --hidden --no-ignore'
+vgh_command='v'
+vgh() ${zsb}.searchFileAndExecute "$0" "$vgh_command" "$vgh_dataGetter" "$@"
+_${zsb}.nocompletion cgh
+
 cg_dataGetter='fd -t f'
 cg_command='c'
 cg() ${zsb}.searchFileAndExecute "$0" "$cg_command" "$cg_dataGetter" "$@"
 compdef "_${zsb}.searchFileAndExecute '${cg_dataGetter}'" cg
 
+cgh_dataGetter='fd -t f --hidden --no-ignore'
+cgh_command='c'
+cgh() ${zsb}.searchFileAndExecute "$0" "$cgh_command" "$cgh_dataGetter" "$@"
+_${zsb}.nocompletion cgh
+
 cdg_dataGetter='fd -t d'
 cdg_command='cd'
 cdg() ${zsb}.searchFileAndExecute "$0" "$cdg_command" "$cdg_dataGetter" "$@"
 compdef "_${zsb}.searchFileAndExecute '${cdg_dataGetter}'" cdg
+
+cdgh_dataGetter='fd -t d --hidden --no-ignore'
+cdgh_command='cd'
+cdgh() ${zsb}.searchFileAndExecute "$0" "$cdgh_command" "$cdgh_dataGetter" "$@"
+_${zsb}.nocompletion cdgh
 
 cdd_dataGetter='dirs -p'
 cdd_command='cd'
