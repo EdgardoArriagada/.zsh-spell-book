02c47afcff11a10bc1c4ccb671fd837934475781
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-01-05 09:13:44 -0300

    remove unecessary code

--- src/utils/close.zsh
+++ src/utils/close.zsh
@@ -1,13 +1,5 @@
 close() {
-  if ! xdotool -version >/dev/null 2>&1; then
-    echo "${ZSB_ERROR} You need to install $(hl xdotool) first"
-    return 1
-  fi
-
-  if [[ -z ${ZSB_TERMINAL_CLOSE_KEY} ]]; then
-    echo " ${ZSB_WARNING} you haven't configured $(hl ZSB_TERMINAL_CLOSE_KEY) var in .env file yet"
-    return 0
-  fi
+  ${zsb}.validate "ZSB_TERMINAL_CLOSE_KEY"
 
   xdotool key ${ZSB_TERMINAL_CLOSE_KEY}
 }
