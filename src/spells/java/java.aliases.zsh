21866fae05908b03bad124b884f1452bc3a26ce0
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-01-07 14:17:28 -0300

    add a few useful aliases

--- src/spells/java/java.aliases.zsh
+++ src/spells/java/java.aliases.zsh
@@ -1,1 +1,2 @@
 alias configurejava='sudo update-alternatives --config java'
+alias javaVersion="java -version 2>&1 | awk -F '\"' 'FNR==2 {print \$2}'"
--- src/spells/zsb/zsb.aliases.zsh
+++ src/spells/zsb/zsb.aliases.zsh
@@ -2,5 +2,6 @@ alias cdzsb="cds ${ZSB_DIR}"
 alias vzsb="cdzsb && nnvim"
 alias galias="alias | rg"
 alias his="nnvim ~/.zsh_history"
+alias hrc="nnvim ~/.zshrc"
 alias env="nnvim ${ZSB_ZSHENV}"
 
