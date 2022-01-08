5b4bd3f0ba7ba8d791812432246241f879deb5ae
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-01-08 10:44:08 -0300

    change printColours output

--- src/utils/printColours.zsh
+++ src/utils/printColours.zsh
@@ -1,5 +1,10 @@
-printcolours() (
+printColours() (
+  integer cols=0
   for i in {0..255}; do
-      printf "\x1b[38;5;${i}mcolour${i}\x1b[0m\n"
+    (( cols++ ))
+    local endLine=''
+    if (( cols % 10 == 0 )); then endLine="\n"; fi
+    printf "\x1b[38;5;${i}m████\x1b[0m►${i}${endLine} "
   done
 )
+
