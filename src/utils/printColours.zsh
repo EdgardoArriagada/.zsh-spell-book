fe61690c21e6ac8aa65b70ce096c87331aa818b8
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-01-08 15:29:44 -0300

    improve printColours performance

--- src/utils/printColours.zsh
+++ src/utils/printColours.zsh
@@ -1,11 +1,22 @@
 printColours() (
+  local this="$0"
   integer cols=0
-  for i in {0..255}; do
-    (( cols++ ))
-    local endLine=''
-    if (( cols % 10 == 0 )); then endLine="\n"; fi
-    local spaces=$(printf %$((3 - ${#i}))s)
-    printf " \x1b[38;5;${i}m████\x1b[0m►${i}${spaces}${endLine}"
-  done
+
+  ${this}.printRange() {
+    local left="$1"
+    local right="$2"
+    local spaces="$3"
+
+    for i in {${left}..${right}}; do
+      (( cols++ ))
+      local endLine=''
+      if (( cols % 10 == 0 )); then endLine="\n"; fi
+      printf " \x1b[38;5;${i}m████\x1b[0m►${i}${spaces}${endLine}"
+    done
+  }
+
+  ${this}.printRange 0 9 '  '
+  ${this}.printRange 10 99 ' '
+  ${this}.printRange 100 255 ''
 )
 
