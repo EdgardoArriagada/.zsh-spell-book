d9fceed561f583eb772e60045dc184c07434ad5f
edgardoarriagada <edgardo.arriagada.ortiz@gmail.com>
2022-01-08 22:23:52 -0300

    fix fd

---
+++ src/setup/fd.setup.zsh
@@ -0,0 +1,2 @@
+(( $ZSB_MACOS )) && return 0
+ln -s $(which fdfind) ~/.local/bin/fd
--- src/spells/ubuntu/ubuntu.aliases.zsh
+++ src/spells/ubuntu/ubuntu.aliases.zsh
@@ -4,7 +4,6 @@ alias upgradea='sudo apt dist-upgrade -y'
 alias remueve='sudo apt purge'
 alias autoremueve='sudo apt clean && sudo apt autoclean && sudo apt autoremove'
 alias mata='sudo killall -9'
-(( $ZSB_MACOS )) || alias fd="fdfind"
 
 alias escapeswap='setxkbmap -option caps:swapescape'
 alias keysbacktonormal='setxkbmap -option'
