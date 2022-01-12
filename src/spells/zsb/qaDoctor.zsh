3341f4f4f299012fab09e4b3374600096460fe81
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-01-11 22:41:34 -0300

    add qaDoctor

---
+++ src/spells/zsb/qaDoctor.zsh
@@ -0,0 +1,32 @@
+qaDoctor() (
+  local file=${1:?'You must provide a file'}
+
+  ${zsb}.doesMatch "$file" "\.qa$" || ${zsb}.throw "File has to be a $(hl ".qa")"
+
+  zmodload zsh/mapfile
+  local fileLines=( "${(f)mapfile[$file]}" )
+
+  local Q='Q--'
+  local A='A--'
+
+  integer tokenCounter=0
+  integer i=0
+
+  for item in $fileLines; do
+    (( i++ ))
+
+    if [[ "$item" = "$Q" ]]; then
+      (( tokenCounter++ ))
+      (( tokenCounter >= 2 )) && ${zsb}.throw "Question duplication in line `hl ${i}`."
+      continue
+    fi
+
+    if [[ "$item" = "$A" ]]; then
+      (( tokenCounter-- ))
+      (( tokenCounter < 0 )) && ${zsb}.throw "Answer duplication in line `hl ${i}`."
+      continue
+    fi
+  done
+
+  ${zsb}.success "`hl ${file}` is ok."
+)
