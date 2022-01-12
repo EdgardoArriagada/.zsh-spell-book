3a2b62889965490c303779f2dffeb7dcb419320f
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-01-11 22:55:43 -0300

    fix current script

--- src/spells/zsb/current.zsh
+++ src/spells/zsb/current.zsh
@@ -1,22 +1,26 @@
-${zsb}.current.getDir() {
+${zsb}.current.getDir() print ${ZSB_TICKETS_DIR}/${ZSB_CURRENT_TICKET}
+
+${zsb}.current.validate() {
   ${zsb}.validate 'ZSB_TICKETS_DIR'
   ${zsb}.validate 'ZSB_CURRENT_TICKET'
-
-  print ${ZSB_TICKETS_DIR}/${ZSB_CURRENT_TICKET}
 }
 
 ${zsb}_createCurrentDir() {
+  ${zsb}.current.validate
+
   local current_ticket_dir=`${zsb}.current.getDir`
 
   [[ ! -d ${current_ticket_dir} ]] && \
     mkdir -p ${current_ticket_dir} && \
-      (cd ${current_ticket_dir}; git init);
+      (builtin cd ${current_ticket_dir} && git init);
+
+  return 0
 }
 
-alias cdcurrent="${zsb}_createCurrentDir; cd `${zsb}.current.getDir`"
-alias vnotescurrent="${zsb}_createCurrentDir; (cdcurrent; nnvim `${zsb}.current.getDir`/NOTES.md)"
-alias cnotescurrent="${zsb}_createCurrentDir; zsb_cat `${zsb}.current.getDir`/NOTES.md"
-alias ncurrent="cdcurrent; (( $ZSB_MACOS )) && open . || nautilus ."
+alias cdcurrent="${zsb}_createCurrentDir && cd `${zsb}.current.getDir`"
+alias vnotescurrent="${zsb}_createCurrentDir && (cdcurrent && nnvim `${zsb}.current.getDir`/NOTES.md)"
+alias cnotescurrent="${zsb}_createCurrentDir && zsb_cat `${zsb}.current.getDir`/NOTES.md"
+alias ncurrent="cdcurrent && (( $ZSB_MACOS )) && open . || nautilus ."
 
 pomodorocurrent() {
   ${zsb}.validate 'ZSB_CURRENT_TICKET'
