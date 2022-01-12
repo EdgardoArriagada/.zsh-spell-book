09f4a26e7acc51f2db5afcb315ff49bbaae3a3a1
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-01-11 23:44:30 -0300

    fix createTsReact

--- src/spells/node/createTsReact.zsh
+++ src/spells/node/createTsReact.zsh
@@ -3,9 +3,9 @@ createTsReact() {
   local -r projectName="$(kebab-case "$args")"
 
   [[ -d $projectName ]] && ${zsb}.throw "$(hl "$1") is already a dir."
-
   eval "npx create-react-app ${projectName} --template typescript" && \
-    yarn add -D prettier && \
+    cd ${projectName} && \
+    eval "yarn add -D prettier" && \
     prettierInit && \
     alert -i 'face-cool' "Happy Hacking" && \
     return 0
