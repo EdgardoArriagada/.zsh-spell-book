5a8f444c7f79799dc3bacbe0c2b3007095fc87cf
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-01-13 20:15:34 -0300

    make viewPomodoroLogs mac compatible

--- src/spells/pomodoro/viewPomodoroLogs.zsh
+++ src/spells/pomodoro/viewPomodoroLogs.zsh
@@ -18,19 +18,30 @@ ${zsb}.pomodoro.viewPomodoroLogs() {
   ${fileOpenerProgram} ${fullFilePath}
 }
 
+${zsb}.pomodoro.twoDaysAgo() {
+  (( $ZSB_MACOS )) && printf '-v -2d' || printf  '-d 2 days ago'
+}
+
+${zsb}.pomodoro.threeDaysAgo() {
+  (( $ZSB_MACOS )) && printf '-v -3d' || printf  '-d 3 days ago'
+}
+
+${zsb}.pomodoro.oneDayAgo() {
+  (( $ZSB_MACOS )) && printf '-v -1d' || printf  '-d 1 day ago'
+}
+
 ${zsb}.pomodoro.lastWorkingDay() {
   local today=`date +%w`
   local SUNDAY=0
   local MONDAY=1
   if [[ $today = $SUNDAY ]] ; then
-    echo "2 days ago"
+    ${zsb}.pomodoro.twoDaysAgo
   elif [[ $today = $MONDAY ]] ; then
-    echo "3 days ago"
+    ${zsb}.pomodoro.threeDaysAgo
   else
-    echo "1 day ago"
+    ${zsb}.pomodoro.oneDayAgo
   fi
 }
-
 
 cpomodoro() ${zsb}.pomodoro.viewPomodoroLogs $(date +%b-%d-%Y.log) zsb_cat
 vpomodoro() ${zsb}.pomodoro.viewPomodoroLogs $(date +%b-%d-%Y.log) nvim
@@ -38,5 +49,10 @@ vpomodoro() ${zsb}.pomodoro.viewPomodoroLogs $(date +%b-%d-%Y.log) nvim
 cypomodoro() ${zsb}.pomodoro.viewPomodoroLogs $(date +%b-%d-%Y.log -d "yesterday") zsb_cat
 vypomodoro() ${zsb}.pomodoro.viewPomodoroLogs $(date +%b-%d-%Y.log -d "yesterday") nvim
 
-cwpomodoro() ${zsb}.pomodoro.viewPomodoroLogs $(date +%b-%d-%Y.log -d "$(${zsb}.pomodoro.lastWorkingDay)") zsb_cat
-vwpomodoro() ${zsb}.pomodoro.viewPomodoroLogs $(date +%b-%d-%Y.log -d "$(${zsb}.pomodoro.lastWorkingDay)") nvim
+if (( $ZSB_MACOS )); then
+  cwpomodoro() ${zsb}.pomodoro.viewPomodoroLogs $(date `${zsb}.pomodoro.lastWorkingDay` +%b-%d-%Y.log) zsb_cat
+  vwpomodoro() ${zsb}.pomodoro.viewPomodoroLogs $(date `${zsb}.pomodoro.lastWorkingDay` +%b-%d-%Y.log) nvim
+else
+  cwpomodoro() ${zsb}.pomodoro.viewPomodoroLogs $(date +%b-%d-%Y.log `${zsb}.pomodoro.lastWorkingDay`) zsb_cat
+  vwpomodoro() ${zsb}.pomodoro.viewPomodoroLogs $(date +%b-%d-%Y.log `${zsb}.pomodoro.lastWorkingDay`) nvim
+fi
