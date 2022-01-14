e82ac9ccf8c3a0233ef46912c9b73594552e786f
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-01-13 21:18:33 -0300

    add default branch security to createPr

--- src/spells/git/createPr.zsh
+++ src/spells/git/createPr.zsh
@@ -1,12 +1,18 @@
 # ghpr.my-repo-folder() printf "--title 'Pr title' --body 'some body to love'"
 
 createPr() {
+  zparseopts -D -E -F -- -aware=aware || return 1
+
   local currDir=${PWD##*/}
   local propsGetter="ghpr.${currDir}"
   ! type "$propsGetter" > /dev/null && ${zsb}.throw "there is not a props getter for `hl ${currDir}`"
 
   local parentBranch=`gitParentBranch`
   [[ -z "$parentBranch" ]] && ${zsb}.cancel "There is not a parentBranch."
+
+  if ${zsb}.userWorkingOnDefaultBranch && [[ -z "$aware" ]]; then
+    ${zsb}.throw "Can't create a pull request on a default branch, use `hl --aware` flag to do it anyway"
+  fi
 
   ${zsb}.warning "Pull request to `hl ${parentBranch}` from `hl $(git branch --show-current)`"
   ${zsb}.confirmMenu.withPrompt
@@ -15,3 +21,5 @@ createPr() {
   gh pr create --draft --base "$parentBranch" ${(z)restOfProps}
   gh pr view --web
 }
+
+compdef "_${zsb}.nonRepeatedList --aware" createPr
