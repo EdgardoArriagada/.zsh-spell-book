5ec9a0a4b4b71b4a7c888ef0f85f5b3119e3114c
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-01-13 21:12:47 -0300

    deduce parent for createPr

--- src/spells/git/createPr.zsh
+++ src/spells/git/createPr.zsh
@@ -1,13 +1,17 @@
-# ghpr.my-repo-folder() printf "--title -WIP --base main"
+# ghpr.my-repo-folder() printf "--title 'Pr title' --body 'some body to love'"
 
 createPr() {
   local currDir=${PWD##*/}
   local propsGetter="ghpr.${currDir}"
   ! type "$propsGetter" > /dev/null && ${zsb}.throw "there is not a props getter for `hl ${currDir}`"
 
+  local parentBranch=`gitParentBranch`
+  [[ -z "$parentBranch" ]] && ${zsb}.cancel "There is not a parentBranch."
+
+  ${zsb}.warning "Pull request to `hl ${parentBranch}` from `hl $(git branch --show-current)`"
   ${zsb}.confirmMenu.withPrompt
 
-  local prProps=`${propsGetter}`
-  gh pr create --draft ${(z)prProps}
+  local restOfProps=`${propsGetter}`
+  gh pr create --draft --base "$parentBranch" ${(z)restOfProps}
   gh pr view --web
 }
---
+++ src/utils/git-utils/gitParent.zsh
@@ -0,0 +1,8 @@
+gitParentBranch() {
+  git show-branch \
+    | sed "s/].*//" \
+    | grep "\*" \
+    | grep -v "$(git rev-parse --abbrev-ref HEAD)" \
+    | head -n1 \
+    | sed "s/^.*\[//"
+}
