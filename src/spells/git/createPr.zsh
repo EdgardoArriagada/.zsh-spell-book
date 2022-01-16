9eddde0083125f230797f017d59a4de47c61855f
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-01-15 22:38:50 -0300

    rearrage createPr script

--- src/spells/git/createPr.zsh
+++ src/spells/git/createPr.zsh
@@ -5,11 +5,8 @@ createPr_defaultProps="--title WIP --body WIP"
 createPr() {
   zparseopts -D -E -F -- -aware=aware || return 1
 
-  local currDir=${PWD##*/}
-  local propsGetter="ghpr.${currDir}"
-
   local parentBranch=`gitParentBranch`
-  [[ -z "$parentBranch" ]] && ${zsb}.cancel "There is not a parentBranch."
+  [[ -z "$parentBranch" ]] && ${zsb}.cancel "There is not a parent branch."
 
   if ${zsb}.userWorkingOnDefaultBranch && [[ -z "$aware" ]]; then
     ${zsb}.throw "Can't create a pull request on a default branch, use `hl --aware` flag to do it anyway"
@@ -18,6 +15,8 @@ createPr() {
   ${zsb}.warning "Pull request to `hl ${parentBranch}` from `hl $(git branch --show-current)`"
   ${zsb}.confirmMenu.withPrompt
 
+  local currDir=${PWD##*/}
+  local propsGetter="ghpr.${currDir}"
   local restOfProps
   if type "$propsGetter" > /dev/null; then
     restOfProps=`${propsGetter}`
