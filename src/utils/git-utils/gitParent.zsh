e11441901009964245a5af0048b0e1277fc7c512
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-01-18 09:45:26 -0300

    fix gitParentBranch

--- src/utils/git-utils/gitParent.zsh
+++ src/utils/git-utils/gitParent.zsh
@@ -2,7 +2,8 @@ gitParentBranch() {
   git show-branch \
     | sed "s/].*//" \
     | grep "\*" \
-    | grep -v "$(git rev-parse --abbrev-ref HEAD)" \
+    | grep -v `git rev-parse --abbrev-ref HEAD` \
     | head -n1 \
-    | sed "s/^.*\[//"
+    | sed "s/^.*\[//" \
+    | cut -d'^' -f1
 }
