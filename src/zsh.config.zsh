8cb4b45f5aeab09746243c301a5e4090cae447c8
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-01-20 23:41:10 -0300

    configure zsh to be able to use vi mode and old mode

--- src/zsh.config.zsh
+++ src/zsh.config.zsh
@@ -1,13 +1,11 @@
+# vi mode
+bindkey -v
+export KEYTIMEOUT=1
+
 autoload throw catch
 
 # Partial tab completion color
 zstyle -e ':completion:*:default' list-colors 'reply=("${PREFIX:+=(#bi)($PREFIX:t)(?)*==34=34}:${(s.:.)LS_COLORS}")';
-
-# Use the vi navigation keys in menu completion
-bindkey -M menuselect 'h' vi-backward-char
-bindkey -M menuselect 'k' vi-up-line-or-history
-bindkey -M menuselect 'l' vi-forward-char
-bindkey -M menuselect 'j' vi-down-line-or-history
 
 ## Autosuggestion plugin
 bindkey '^ ' autosuggest-accept
@@ -17,13 +15,48 @@ typeset -Ag ZSH_HIGHLIGHT_STYLES
 
 # To differentiate aliases and functions from other command types
 ZSH_HIGHLIGHT_STYLES[alias]='fg=magenta,bold'
-ZSH_HIGHLIGHT_STYLES[function]='fg=magenta,bold'
+ZSH_HIGHLIGHT_STYLES[function]='fg=blue,bold'
 
 # To have paths colored instead of underlined
 ZSH_HIGHLIGHT_STYLES[path]='fg=blue'
 
 # Misc
 ZSH_HIGHLIGHT_STYLES[comment]='fg=cyan'
+
+ZSB_CURSOR_DEFAULT='\e[6 q'
+ZSB_CURSOR_VIM='\e[1 q'
+
+# Change cursor shape for different vi modes.
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]] ||
+     [[ $1 = 'block' ]]; then
+    echo -ne "$ZSB_CURSOR_VIM"
+  elif [[ ${KEYMAP} == main ]] ||
+       [[ ${KEYMAP} == viins ]] ||
+       [[ ${KEYMAP} = '' ]] ||
+       [[ $1 = 'beam' ]]; then
+    echo -ne "$ZSB_CURSOR_DEFAULT"
+  fi
+}
+zle -N zle-keymap-select
+zle-line-init() {
+    zle vi-insert # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+    echo -ne "$ZSB_CURSOR_DEFAULT"
+}
+zle -N zle-line-init
+echo -ne "$ZSB_CURSOR_DEFAULT" # Use beam shape cursor on startup.
+preexec() { echo -ne "$ZSB_CURSOR_DEFAULT"; } # Use beam shape cursor for each new prompt.
+
+function append-last-word { ((++CURSOR)); zle insert-last-word; zle vi-insert; }
+zle -N append-last-word
+
+bindkey -M vicmd . append-last-word
+bindkey -M viins '^a' beginning-of-line
+bindkey -M viins '^p' up-history
+bindkey -M viins '^n' down-history
+bindkey -M viins '^e' end-of-line
+bindkey -M viins '^[d' kill-word
+bindkey -M viins '^w' backward-kill-word
 
 prompt_context() {
   local emojis=(⚡️ 🔥 💀 👑 😎 🦄 🌈 🚀 💡 🎉 🌙 🍟 🍔 🌎 ⛄ 🌞 🪐 🔱 🎩 🛸 🎬 🏹 🧜)
