fe93d6be303e72a587df8f4b97b3bc03934fe8e2
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-01-20 23:55:00 -0300

    improve C-p and C-n in zsh

--- src/zsh.config.zsh
+++ src/zsh.config.zsh
@@ -52,8 +52,8 @@ zle -N append-last-word
 
 bindkey -M vicmd . append-last-word
 bindkey -M viins '^a' beginning-of-line
-bindkey -M viins '^p' up-history
-bindkey -M viins '^n' down-history
+bindkey -M viins '^p' up-line-or-search
+bindkey -M viins '^n' down-line-or-search
 bindkey -M viins '^e' end-of-line
 bindkey -M viins '^[d' kill-word
 bindkey -M viins '^w' backward-kill-word
