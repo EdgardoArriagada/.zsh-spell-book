fed8ee4f96e8841663f89e73f005f0e8c3e06214
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-01-28 12:45:00 -0300

    remove no longer used ruby stuff

--- src/configurations/ruby-conf/ruby.conf.zsh
+++
@@ -1,9 +0,0 @@
-# rbenv ubuntu https://github.com/rbenv/rbenv
-if [[ -d "$HOME/.rbenv" ]]; then
-  __${zsb}.prepareLazyLoad "$ZSB_DIR/src/configurations/ruby-conf/rbenv.init" \
-    rbenv ruby gem irb bundle rspec yard solargraph
-
-  __${zsb}.prepareLazyLoad "$ZSB_DIR/src/configurations/ruby-conf/rails.init" \
-    rails rake
-fi
-
