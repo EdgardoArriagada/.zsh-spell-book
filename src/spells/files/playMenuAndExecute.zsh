c84107de59bef1bf28796d2491ec96f9b2e57ca8
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-02-01 19:09:54 -0300

    make sure some helper vars do not appear as tab suggested commands

--- src/spells/files/playMenuAndExecute.zsh
+++ src/spells/files/playMenuAndExecute.zsh
@@ -21,46 +21,46 @@ _${zsb}.playMenuAndExecute() {
 
 compdef _${zsb}.playMenuAndExecute ${zsb}.playMenuAndExecute
 
-vg_dataGetter='fd -t f'
+__vg_dataGetter='fd -t f'
 vg_command='v'
-vg() ${zsb}.playMenuAndExecute "$0" "$vg_command" "$vg_dataGetter" "$@"
-compdef "_${zsb}.playMenuAndExecute '${vg_dataGetter}'" vg
+vg() ${zsb}.playMenuAndExecute "$0" "$vg_command" "$__vg_dataGetter" "$@"
+compdef "_${zsb}.playMenuAndExecute '${__vg_dataGetter}'" vg
 
-vgh_dataGetter='fd -t f --hidden --no-ignore'
-vgh_command='v'
-vgh() ${zsb}.playMenuAndExecute "$0" "$vgh_command" "$vgh_dataGetter" "$@"
+__vgh_dataGetter='fd -t f --hidden --no-ignore'
+__vgh_command='v'
+vgh() ${zsb}.playMenuAndExecute "$0" "$__vgh_command" "$__vgh_dataGetter" "$@"
 _${zsb}.nocompletion cgh
 
-cg_dataGetter='fd -t f'
-cg_command='c'
-cg() ${zsb}.playMenuAndExecute "$0" "$cg_command" "$cg_dataGetter" "$@"
-compdef "_${zsb}.playMenuAndExecute '${cg_dataGetter}'" cg
+__cg_dataGetter='fd -t f'
+__cg_command='c'
+cg() ${zsb}.playMenuAndExecute "$0" "$__cg_command" "$__cg_dataGetter" "$@"
+compdef "_${zsb}.playMenuAndExecute '${__cg_dataGetter}'" cg
 
-cgh_dataGetter='fd -t f --hidden --no-ignore'
-cgh_command='c'
-cgh() ${zsb}.playMenuAndExecute "$0" "$cgh_command" "$cgh_dataGetter" "$@"
+__cgh_dataGetter='fd -t f --hidden --no-ignore'
+__cgh_command='c'
+cgh() ${zsb}.playMenuAndExecute "$0" "$__cgh_command" "$__cgh_dataGetter" "$@"
 _${zsb}.nocompletion cgh
 
-cdg_dataGetter='fd -t d'
-cdg_command='cd'
-cdg() ${zsb}.playMenuAndExecute "$0" "$cdg_command" "$cdg_dataGetter" "$@"
-compdef "_${zsb}.playMenuAndExecute '${cdg_dataGetter}'" cdg
+__cdg_dataGetter='fd -t d'
+__cdg_command='cd'
+cdg() ${zsb}.playMenuAndExecute "$0" "$__cdg_command" "$__cdg_dataGetter" "$@"
+compdef "_${zsb}.playMenuAndExecute '${__cdg_dataGetter}'" cdg
 
-cdgh_dataGetter='fd -t d --hidden --no-ignore'
-cdgh_command='cd'
-cdgh() ${zsb}.playMenuAndExecute "$0" "$cdgh_command" "$cdgh_dataGetter" "$@"
+__cdgh_dataGetter='fd -t d --hidden --no-ignore'
+__cdgh_command='cd'
+cdgh() ${zsb}.playMenuAndExecute "$0" "$__cdgh_command" "$__cdgh_dataGetter" "$@"
 _${zsb}.nocompletion cdgh
 
-cdd_dataGetter='dirs -p'
-cdd_command='cd'
-cdd() ${zsb}.playMenuAndExecute "$0" "$cdd_command" "$cdd_dataGetter" "$@"
-compdef "_${zsb}.playMenuAndExecute '${cdd_dataGetter}'" cdd
+__cdd_dataGetter='dirs -p'
+__cdd_command='cd'
+cdd() ${zsb}.playMenuAndExecute "$0" "$__cdd_command" "$__cdd_dataGetter" "$@"
+compdef "_${zsb}.playMenuAndExecute '${__cdd_dataGetter}'" cdd
 
-cdp_dataGetter='fd'
-cdp_command='cd'
+__cdp_dataGetter='fd'
+__cdp_command='cd'
 cdp() {
   if ! [[ -a "$1" ]]; then
-    ${zsb}.playMenuAndExecute "$0" "$cdp_command" "$cdp_dataGetter" "$@"
+    ${zsb}.playMenuAndExecute "$0" "$__cdp_command" "$__cdp_dataGetter" "$@"
     return 0
   fi
 
@@ -72,5 +72,5 @@ cdp() {
     eval "cd $(dirname ${1})"; return 0
   fi
 }
-compdef "_${zsb}.playMenuAndExecute '${cdp_dataGetter}'" cdp
+compdef "_${zsb}.playMenuAndExecute '${__cdp_dataGetter}'" cdp
 
