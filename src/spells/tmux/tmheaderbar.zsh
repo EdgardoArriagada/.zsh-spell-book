7cbb34eeee5bf24cb986dc74923e506944fe2a51
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-02-03 15:51:07 -0300

    improve tmheadbar by autodestroying it on exit

--- src/spells/tmux/tmheaderbar.zsh
+++ src/spells/tmux/tmheaderbar.zsh
@@ -1,5 +1,5 @@
 tmheaderbar() {
   tmux split-window -vb -p 6 -t 0 \; \
-    send-keys -t 0 " printCentered \"$*\" | less " Enter \; \
+    send-keys -t 0 " printCentered \"$*\" | less; exit " Enter \; \
     select-pane -t 1
 }
