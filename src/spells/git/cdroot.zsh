ba46439e62468745d6cfae981cbd734a7187de88
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-02-08 11:49:27 -0300

    mv cdroot to spells

--- src/spells/git/cdroot.zsh
+++ src/spells/git/cdroot.zsh
@@ -2,3 +2,8 @@ cdroot() {
   local pathToDotGit=`git rev-parse --git-dir 2>/dev/null`
   [[ -n "$pathToDotGit" ]] && cd `dirname ${pathToDotGit}`
 }
+
+hisIgnore cdroot
+
+_${zsb}.nocompletion cdroot
+
