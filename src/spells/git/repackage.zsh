5c432b657ce7dcc2c7fdbc221a20ee1ccb6cfcdf
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-02-22 20:32:21 -0300

    merge relabel with repackage

--- src/spells/git/repackage.zsh
+++ src/spells/git/repackage.zsh
@@ -2,14 +2,19 @@ repackage() {
   zparseopts -D -E -F -- -aware=aware -force=force || return 1
 
   if ${zsb}.userWorkingOnDefaultBranch && [[ -z "$aware" ]]; then
-    ${zsb}.throw "Can't repackage into default branch, use $(hl --aware) flag to do it anyway"
+    ${zsb}.throw "can't repackage in default branch, use $(hl --aware) flag to do it anyway"
   fi
 
-  if ${zsb}.isLastCommitOnline && [[ -z "$force" ]]; then
-    ${zsb}.throw "Can't repackage, HEAD commit has already been pushed online, use $(hl --force) flag to do it anyway"
+  if ${zsb}.isLastCommitOnline && ! [[ -z "$force" ]]; then
+    ${zsb}.throw "Can't repackage, HEAD commit has already been pushed online, use $(hl --force) flag to do it anyway."
   fi
 
-  git commit --amend --no-edit --gpg-sign && ${zsb}.gitStatus
+  if [[ -n "$1" ]]; then
+    git commit --amend --gpg-sign --message "$*" && ${zsb}.gitStatus &&
+      ${zsb}.warning "Files already added to git may have been commited, use $(hl "git reset HEAD~") to undo the entire previous commit."
+  else
+    git commit --amend --gpg-sign --message && ${zsb}.gitStatus
+  fi
 }
 
 hisIgnore repackage
