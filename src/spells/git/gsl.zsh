3b3f1774c8757bd2732a6b101909cc1fd885d28e
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-03-03 12:22:50 -0300

    add a flag to gsl to clear list

--- src/spells/git/gsl.zsh
+++ src/spells/git/gsl.zsh
@@ -1,11 +1,22 @@
 gsl() {
-  local -r stashList="$(git stash list)"
-  [[ -z "$stashList" ]] && ${zsb}.cancel "Stash list is empty."
+  local gslEmptyMsg="Stash list is empty."
+  local -r stashList="`git stash list`"
+
+  [[ -z "$stashList" ]] && ${zsb}.cancel "$gslEmptyMsg"
+
+  if [[ "$1" = "--clear" ]]; then
+    ${zsb}.warning "You are about to clear the `hl 'git stash list'`"
+    ${zsb}.confirmMenu.withPrompt
+
+    git stash clear && ${zsb}.success "$gslEmptyMsg"
+    return 0
+  fi
+
   echo "$stashList" | c -p -l ruby -
 }
 
-hisIgnore gsl
+hisIgnore gsl 'gsl --clear'
+
+compdef "_${zsb}.singleComp '--clear'" gsl
 
 alias GSL="toggleCapsLock && gsl"
-
-_${zsb}.nocompletion gsl
