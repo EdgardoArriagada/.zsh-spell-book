1b391e66f1fc66c4c72862f91c4542b951cbe35d
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-03-06 02:53:54 -0300

    simplofy parentBranch command

--- src/utils/git-utils/parentBranch.zsh
+++ src/utils/git-utils/parentBranch.zsh
@@ -1,9 +1,7 @@
 parentBranch () {
   git log --first-parent --pretty='%d' \
     | grep 'origin\/' \
-    | sed 's/(tag: .*)//g' \
-    | sed '/^[[:space:]]*$/d' \
     | head -2 \
     | tail -1 \
-    | sed 's/[()]//g' | sed 's/,.*//g' | sed 's/origin\///g'
+    | sed 's/,.*//g' | sed 's/origin\///g' | sed 's/[()]//g'
 }
