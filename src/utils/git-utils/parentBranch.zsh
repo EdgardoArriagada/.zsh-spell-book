5ddefa15054cfdbc5f13c9ea474b4f0a621cb732
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-03-06 11:49:56 -0300

    improve parentBranch with some rust programs

--- src/utils/git-utils/parentBranch.zsh
+++ src/utils/git-utils/parentBranch.zsh
@@ -1,11 +1,11 @@
 parentBranch () {
   git log --first-parent --pretty='%D' \
     | awk '(NR>1)' \
-    | grep 'origin\/' \
-    | sed 's/$/,/g' \
-    | perl -pe 's|tag: .+?,||g' \
+    | rg 'origin/' \
+    | sd '$' ',' \
+    | sd 'tag: .+?,' '' \
     | head -1 \
-    | sed 's/,.*//g' \
-    | sed 's/origin\///g' \
+    | sd ',.*' '' \
+    | sd 'origin/' '' \
     | xargs
 }
