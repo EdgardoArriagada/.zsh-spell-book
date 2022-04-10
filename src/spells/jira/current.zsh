02c8b4d23d44f4009d97a81a312a762f93088c29
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-04-09 21:20:47 -0400

    move current to a new folder

---
+++ src/spells/jira/current.zsh
@@ -0,0 +1,34 @@
+${zsb}.current.getDir() print ${ZSB_TICKETS_DIR}/${ZSB_PARENT_TICKET}
+
+${zsb}.current.validate() {
+  ${zsb}.validate 'ZSB_TICKETS_DIR'
+  ${zsb}.validate 'ZSB_CURRENT_TICKET'
+  ${zsb}.validate 'ZSB_PARENT_TICKET'
+}
+
+${zsb}_createCurrentDir() {
+  ${zsb}.current.validate
+
+  local parentTicketDir=`${zsb}.current.getDir`
+
+  [[ ! -d ${parentTicketDir} ]] && \
+    mkdir -p ${parentTicketDir} && \
+      (builtin cd ${parentTicketDir} && git init);
+
+  return 0
+}
+
+alias cdcurrent="${zsb}_createCurrentDir && cd `${zsb}.current.getDir`"
+alias vnotescurrent="${zsb}_createCurrentDir && (cdcurrent && nnvim `${zsb}.current.getDir`/NOTES.md)"
+alias cnotescurrent="${zsb}_createCurrentDir && zsb_cat `${zsb}.current.getDir`/NOTES.md"
+alias ncurrent="cdcurrent && (( $ZSB_MACOS )) && open . || nautilus ."
+
+hisIgnore cdcurrent vnotescurrent cnotescurrent ncurrent
+
+pomodorocurrent() {
+  ${zsb}.validate 'ZSB_CURRENT_TICKET'
+  local inputTime="$1"
+  shift 1
+  ${zsb}.info "Current ticket: `hl ${ZSB_CURRENT_TICKET}`"
+  pomodoro "$inputTime" "$ZSB_CURRENT_TICKET" "$*"
+}
