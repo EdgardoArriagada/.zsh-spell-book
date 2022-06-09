6c8694a2916c0cf85329aebf1e807bd68d681e36
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-06-08 22:39:14 -0400

    update git config to behave like before purging zsh

--- src/setup/git.setup.zsh
+++ src/setup/git.setup.zsh
@@ -1,15 +1,15 @@
 if git --version >/dev/null 2>&1; then
   # configuration goes to ~/.gitconfig
 
-  git config --global core.editor "vim"
+  git config --global core.editor "nvim"
   git config --global --replace-all core.excludesfile ${ZSB_DIR}/src/dotFiles/git/global.gitignore
   git config --global pull.rebase false
 
   git config --global pager.branch false # print branch info instead of "less" it
-  git config --global pager.diff 'delta | less'
-  git config --global pager.log 'delta | less'
+  git config --global pager.diff 'delta | less -+F -+X'
+  git config --global pager.log 'delta | less -+F -+X'
   git config --global pager.reflog 'delta'
-  git config --global pager.show 'delta | less'
+  git config --global pager.show 'delta | less -+F -+X'
 
   git config --global interactive.diffFilter 'delta --color-only'
 
