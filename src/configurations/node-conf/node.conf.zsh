98b07ab758be879c55cc25da033f2fc9b3f2f37c
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-06-20 21:10:15 -0400

    replace nvm with fnm

--- src/configurations/node-conf/node.conf.zsh
+++ src/configurations/node-conf/node.conf.zsh
@@ -1,26 +1,4 @@
-### lazy load nvm
-
-# for this to work, make sure you
-# go to https://github.com/nvm-sh/nvm
-# and follow the 'Manual Install' section
-
-export NVM_DIR="$HOME/.nvm"
-
-${zsb}.lazyNvm() {
-  (( $ZSB_MACOS )) && \
-    source "$(brew --prefix nvm)/nvm.sh" || \
-    source "$NVM_DIR/nvm.sh"
-
-  ${zsb}.removeNodeVersionCacheDecorator nvm
-}
-
-if [[ -d "$NVM_DIR" ]]; then
-  local nvmScriptPath
-
-  __${zsb}.prepareLazyLoad "${zsb}.lazyNvm" \
-    nvm yarn node npm vue npx tsc depcheck expo nest markdown-pdf
-fi
-
+eval "$(fnm env --use-on-cd)"
 ### To run global npm packages
 export PATH=~/.npm-global/bin:$PATH
 
