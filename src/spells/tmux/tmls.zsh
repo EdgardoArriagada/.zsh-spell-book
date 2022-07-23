be591261b3f78af3c1ea61de7fe26e837d23c67d
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-07-22 23:24:57 -0400

    unify tmls commands

---
+++ src/spells/tmux/tmls.zsh
@@ -0,0 +1,16 @@
+# zsh completions does not read aliases, so tmls is a function
+tmls() { tmux ls 2>&1 | cut -d':' -s -f1 }
+
+tml() {
+  local latestSession=$(tmux display-message -p '#S')
+  local currentList=(`eval "tmls"`)
+  printf "\n"
+  for session in "${currentList[@]}"; do
+    if [[ "$session" = "$latestSession" ]]; then
+      [[ -z "$TMUX" ]] && printf "~ ${session}\n" || printf "* $(hl ${session})\n"
+    else
+      printf "  ${session}\n"
+    fi
+  done
+  printf "\n"
+}
--- src/spells/tmux/tmux.aliases.zsh
+++
@@ -1,3 +0,0 @@
-# zsh completions does not read aliases, so tmls is a function
-tmls() { tmux ls 2>&1 | cut -d':' -s -f1 }
-
