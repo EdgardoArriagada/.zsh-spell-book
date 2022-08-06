dea5bc9eb628e7ae86a2e16b01461b3779e76093
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-08-05 22:43:47 -0400

    improve page to be able to specify page

--- src/spells/notebook/notebook.zsh
+++ src/spells/notebook/notebook.zsh
@@ -3,7 +3,7 @@ ${zsb}.page.validateEnv() {
   ${zsb}.validate "ZSB_NOTEBOOK_CHAPTER"
 }
 
-${zsb}.page.getCurrentPage() {
+${zsb}.page.getTodaysPage() {
   local year=$(date +%Y)
   local month=$(date +%b)
   local page=$(date +%b-%d-%Y.md)
@@ -14,12 +14,25 @@ ${zsb}.page.getCurrentPage() {
   print ${currentPage}
 }
 
-${zsb}.page() {${zsb}.page.validateEnv && ${@} $(${zsb}.page.getCurrentPage)}
-
-cdpage() {${zsb}.page.validateEnv && cds $(dirname $(${zsb}.page.getCurrentPage))}
-
 alias cdnotebook="${zsb}.validate 'ZSB_NOTEBOOK_DIR' && cds ${ZSB_NOTEBOOK_DIR}"
-alias page="(cdpage && ${zsb}.page nvim)"
+
+${zsb}.page() {
+  local cmd="$1"
+  local currentPage="$2"
+  ${zsb}.page.validateEnv
+
+  if [[ -z "$currentPage" ]]; then
+    local todaysPage=`${zsb}.page.getTodaysPage`
+
+    (builtin cd $(dirname ${todaysPage}) && \
+      ${cmd} ${todaysPage})
+  else
+    (builtin cd ${ZSB_NOTEBOOK_DIR}/${ZSB_NOTEBOOK_CHAPTER} && \
+      ${cmd} "$currentPage")
+  fi
+}
+
+alias page="${zsb}.page nvim"
 alias cpage="${zsb}.page zsb_cat"
 alias ccppage="${zsb}.page ccp"
 
