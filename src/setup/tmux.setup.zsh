f435c5c1c1ba7751a8a699304fc9e620ebb4f815
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-08-13 13:00:05 -0400

    tmux sourcing at the top of zshc for performance

--- src/setup/tmux.setup.zsh
+++ src/setup/tmux.setup.zsh
@@ -1,2 +1,11 @@
 ${zsb}.safeLink tmux/tmux.conf ~/.tmux.conf
 
+# Put tmux at the beggining of ~/.zshrc 
+if (( $ZSB_MACOS ))
+  then sed -i '.bak' '1s/^/[[ -z "$TMUX" ]] \&\& tmux new -A -s main\'$'\n/g' ~/.zshrc
+  else sed -i '1i[[ -z "$TMUX" ]] \&\& tmux new -A -s main' ~/.zshrc
+fi
+
+
+
+
