04343ac769ad943039da2060f61ffd6dda8eae4f
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-08-28 00:20:02 -0400

    apply a small performance refactor

--- src/utils/git-utils/git.completion.zsh
+++ src/utils/git-utils/git.completion.zsh
@@ -6,30 +6,36 @@ ${zsb}.gitBranches() {
   esac
 }
 
+${zsb}.getGitFiles.removeGitTokens() sd '^...' '$1'
+
+# '--short' is better than '--porcelain' because
+# it keeps the paths of the git files relative
+# to current working folder
+${zsb}.getGitFiles.gitShortStatus() git status --short 2>/dev/null
+
+${zsb}.getGitFiles.getgitflesfromregex() {
+  ${zsb}.getGitFiles.gitShortStatus | rg "$1" | ${zsb}.getGitFiles.removeGitTokens
+}
+
+declare -gAr ZSB_FILE_TOCMD=(
+  ['staged']="${zsb}.getGitFiles.getgitflesfromregex '^[MARCD]'"
+  ['green']="${zsb}.getGitFiles.getgitflesfromregex '^[MARCD]'"
+  ['unstaged']="${zsb}.getGitFiles.getgitflesfromregex '^.[MARCD]'"
+  ['untracked']="${zsb}.getGitFiles.getgitflesfromregex '^\?{2}'"
+  ['red-safe']="${zsb}.getGitFiles.getgitflesfromregex '^.[MARCD\?]'"
+  ['red']="${zsb}.getGitFiles.getgitflesfromregex '^.[MARCUD\?]'"
+  ['red-with-diff']="${zsb}.getGitFiles.getgitflesfromregex '^.[MARCUD]'"
+  ['unmerged']="${zsb}.getGitFiles.getgitflesfromregex '(^U)|(^.U)'"
+)
+
 ${zsb}.getGitFiles() (
   local this="$0"
+  local newCmd="${ZSB_FILE_TOCMD[$1]}"
 
-  ${this}.getgitflesfromregex() {
-    ${this}.gitShortStatus | rg "$1" | ${this}.removeGitTokens
-  }
-
-  # '--short' is better than '--porcelain' because
-  # it keeps the paths of the git files relative
-  # to current working folder
-  ${this}.gitShortStatus() git status --short 2>/dev/null
-
-  ${this}.removeGitTokens() sd '^...' '$1'
-
-  case "$1" in
-    'staged'|'green') ${this}.getgitflesfromregex '^[MARCD]' ;;
-    'unstaged') ${this}.getgitflesfromregex '^.[MARCD]' ;;
-    'untracked') ${this}.getgitflesfromregex '^\?{2}' ;;
-    'red-safe') ${this}.getgitflesfromregex '^.[MARCD\?]' ;;
-    'red') ${this}.getgitflesfromregex '^.[MARCUD\?]' ;;
-    'red-with-diff') ${this}.getgitflesfromregex '^.[MARCUD]' ;;
-    'unmerged') ${this}.getgitflesfromregex '(^U)|(^.U)' ;;
-    *) ${this}.gitShortStatus | ${this}.removeGitTokens ;;
-  esac
+  if [[ -n "$newCmd" ]]
+    then eval ${newCmd}
+    else ${this}.gitShortStatus | ${this}.removeGitTokens
+  fi
 )
 
 _${zsb}.gitUnrepeat() {
