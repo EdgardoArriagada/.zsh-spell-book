01cc50fb2837affcabe77196f595576b9254d508
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-09-03 16:36:36 -0400

    add a new usefull visualezeAll spell

---
+++ src/spells/files/visualizeAll.zsh
@@ -0,0 +1,11 @@
+visualizeAll() {
+  local -r extension=${1:?'You must provide a file extension'}
+
+  for file in ./*.${extension}; do
+    ${zsb}.info "Visualizing ${file}"
+    eval "v ${file}"
+  done
+}
+
+# TODO: complete with all file extensions in current directory
+
