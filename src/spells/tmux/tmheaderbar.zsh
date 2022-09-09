c97ff5882cbef12ea46f9cc2a18a6d194d630991
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-09-08 21:03:02 -0400

    add a fixed tmheadbar for slow commands

--- src/spells/tmux/tmheaderbar.zsh
+++ src/spells/tmux/tmheaderbar.zsh
@@ -3,3 +3,15 @@ tmheaderbar() {
     send-keys -t 0 " printCentered \"${*}\" | less; exit " Enter \; \
     select-pane -t 1
 }
+
+# param $1: sessionName=`tmux display-message -p '#S'`
+# param $2: windowNumber=`tmux display-message -p '#I'`
+tmheaderbarFixed() {
+  local givenWindow="${1}:${2}"
+  shift 2
+
+  tmux split-window -vb -p 6 -t ${givenWindow}.0 \; \
+    send-keys -t ${givenWindow}.0 " printCentered \"${*}\" | less; exit " Enter \; \
+    select-pane -t ${givenWindow}.1
+
+}
