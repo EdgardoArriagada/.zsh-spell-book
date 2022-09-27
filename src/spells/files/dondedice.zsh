27ab042fcc5e52ac343df7143c05d3acffbead33
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-09-27 17:02:21 -0300

    boy scout

--- src/spells/files/dondedice.zsh
+++ src/spells/files/dondedice.zsh
@@ -1,10 +1,10 @@
+# $1 is a pattern or a comma separated list of blobs
 dondedice() {
   [[ -z "$1" ]] && return 1
 
-  if [[ "$#" == "1" ]]; then
-    rg -g '!{package-lock.json}' "$1"
-    return $? 
+  if [[ "$#" = "1" ]]
+    then rg -g '!{package-lock.json}' "$1"
+    else rg -g "!{package-lock.json,${1}}" "${@:2}"
   fi
 
-  rg -g "!{package-lock.json,${1}}" "${@:2}"
 }
