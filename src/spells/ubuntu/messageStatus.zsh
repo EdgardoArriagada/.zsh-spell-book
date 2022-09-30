bf522490ea33bd955fee3db27e0949b7c988f202
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-09-29 21:00:35 -0300

    refactor messageStatus

--- src/spells/ubuntu/messageStatus.zsh
+++ src/spells/ubuntu/messageStatus.zsh
@@ -1,11 +1,10 @@
 messageStatus() {
   local msg="\e[38;5;244m${@}\033[0m"
 
-  "$@"
+  ${@}
 
-  (( $? )) && \
-    printf "\r\e[38;5;1m✘\033[0m ${msg}\n" && \
-    return 0
-
-  printf "\r\e[38;5;2m✔\033[0m ${msg}\n"
+  if (( $?))
+    then printf "\r\e[38;5;1m✘\033[0m ${msg}\n"
+    else printf "\r\e[38;5;2m✔\033[0m ${msg}\n"
+  fi
 }
