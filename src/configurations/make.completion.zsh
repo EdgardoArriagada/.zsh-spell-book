e25d967a174ff5c6940430b6ae8f4cd9d8adcde9
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-11-04 22:23:25 -0300

    improve make completion

--- src/configurations/make.completion.zsh
+++ src/configurations/make.completion.zsh
@@ -2,11 +2,10 @@ _${zsb}.make() {
   [[ "$CURRENT" -gt 2 ]] && return 0
   [[ ! -f ./Makefile ]] && return 0
 
-  local -r parsedMakefile="$(
-    grep -C 1 '^[a-zA-Z]' Makefile |\
-    sed -E ':a ; $!N ; s/\n\s+// ; ta ; P ; D' |\
-    sed -E ':a;$!N;s/\n--/ \(\.\.\.\)/;ta;P;D'
-  )"
+  local -r parsedMakefile="`
+      grep --only-matching '^[a-zA-Z-]*:' Makefile |\
+      sd ':' ''
+  `"
 
   local -r compList=( "${(@f)parsedMakefile}" )
 
