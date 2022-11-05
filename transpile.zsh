b6390e4a5c635163b5ab2d353e244f77944f8547
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-11-05 16:32:19 -0300

    support transpiled version

---
+++ transpile.zsh
@@ -0,0 +1,35 @@
+rm transpiled.zsh
+rm temp_transpilation.zsh
+# Root global variables
+ZSB_DIR=`dirname ${0}`
+ZSB_TEMP_DIR=${ZSB_DIR}/src/temp
+print "ZSB_DIR=`dirname ${0}`" >> temp_transpilation.zsh
+print "ZSB_TEMP_DIR=${ZSB_DIR}/src/temp" >> temp_transpilation.zsh
+
+# Dynamic prefix
+: ${zsb:='zsb'}
+
+${zsb}.sourceFiles() for f in ${*}; do cat ${f} >> temp_transpilation.zsh; done
+
+() {
+  # Source environment variables
+  local envFile=${ZSB_DIR}/.env
+  [[ -f ${envFile} ]] && cat ${envFile} >> temp_transpilation.zsh
+  cat ${ZSB_DIR}/src/zsh.config.zsh >> temp_transpilation.zsh
+  cat ${ZSB_DIR}/src/globalVariables.zsh >> temp_transpilation.zsh
+
+  # Source files in this specific order
+  ${zsb}.sourceFiles ${ZSB_DIR}/src/utils/**/*.zsh
+  ${zsb}.sourceFiles ${ZSB_DIR}/src/configurations/**/*.zsh
+  ${zsb}.sourceFiles ${ZSB_DIR}/src/spells/**/*.zsh
+
+  # Temporal Spells
+  local tempSpells=( ${ZSB_DIR}/src/temp/spells**/*.zsh )>/dev/null 2>&1
+  [[ -n "$tempSpells" ]] && ${zsb}.sourceFiles ${tempSpells}
+
+  ${zsb}.sourceFiles ${ZSB_DIR}/src/automatic-calls/**/*.zsh
+}
+
+sed '/^$/d' temp_transpilation.zsh > transpiled.zsh
+sd '\$\{zsb\}' "${zsb}" transpiled.zsh
+rm temp_transpilation.zsh
