f2c44312e68a526a2432e66fdb118f8a0882d3ad
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-01-11 12:13:37 -0300

    apply a small refactor

--- src/spells/files/dondedice.zsh
+++ src/spells/files/dondedice.zsh
@@ -1,10 +1,8 @@
-# $1 is a pattern or a comma separated list of blobs
+# when $# > 1, $1 is a pattern or a comma separated list of blobs
 dondedice() {
-  [[ -z "$1" ]] && return 1
-
-  if [[ "$#" = "1" ]]
-    then rg -g '!{package-lock.json}' "$1"
-    else rg -g "!{package-lock.json,${1}}" "${@:2}"
-  fi
-
+  case $# in
+    0) return 1 ;;
+    1) rg -g '!{package-lock.json}' ${1} ;;
+    *) rg -g "!{package-lock.json,${1}}" "${@:2}" ;;
+  esac
 }
