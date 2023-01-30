b2c2a46d01e392ec39684a6a1c0dc5d6f564ec47
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-01-30 09:08:49 -0300

    migrate sxhkd and skhd

--- src/setup/sxhkd.setup.zsh
+++
@@ -1,7 +0,0 @@
-if (( $ZSB_MACOS )); then
-  ${zsb}.safeLink skhd/.skhdrc ~/.skhdrc
-else
-  ${zsb}.safeLink sxhkd/sxhkd.desktop ~/.config/autostart/sxhkd.desktop
-
-  ${zsb}.safeLink sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
-fi
