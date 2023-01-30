c1fe9295ae3f40114448f6d7174cc43d553d0bbc
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-01-30 19:56:23 -0300

    migrate vscode

--- src/setup/vscode.setup.zsh
+++
@@ -1,4 +0,0 @@
-${zsb}.safeLink vscode/settings.json $CONFIG/Code/User/settings.json
-${zsb}.safeLink vscode/keybindings.json $CONFIG/Code/User/keybindings.json
-${zsb}.safeLink vscode/snippets $CONFIG/Code/User/snippets
-
