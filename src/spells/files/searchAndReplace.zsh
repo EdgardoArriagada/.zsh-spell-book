3b6943b9e32529f893e7fb81ed09196fba66229b
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-02-07 21:11:29 -0300

    improve DX of searchAndReplace

--- src/spells/files/searchAndReplace.zsh
+++ src/spells/files/searchAndReplace.zsh
@@ -1,8 +1,19 @@
 searchAndReplace() {
-  local search=$1
-  local replace=$2
+  local search=${1:?Search string is required}
+  local replace=${2:?Replace string is required}
 
-  sd ${search} ${replace} `rg --files-with-matches ${search}`
+  local filesWithMatches=(`rg --files-with-matches ${search}`)
+
+  if (( ${#filesWithMatches[@]} == 0 )); then
+    ${zsb}.throw "No files found with matches for '`hl ${search}`'"
+    return 1
+  fi
+
+  ${zsb}.confirmMenu.withPrompt
+
+  sd ${search} ${replace} ${filesWithMatches[@]}
+
+  (( $? == 0 )) && ${zsb}.success "`hl ${search}` -> `hl ${replace}`"
 }
 
 
