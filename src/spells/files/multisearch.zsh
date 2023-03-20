4b4dfbe4245b9b8b30efbcdf9df5189209822de4
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-03-19 23:47:37 -0300

    fix multisearch with two args and first one failing

--- src/spells/files/multisearch.zsh
+++ src/spells/files/multisearch.zsh
@@ -7,11 +7,13 @@ multisearch() {
   local lastArg="${@[$#]}"
   local files=(`rg --files-with-matches ${1}`)
 
+  (( ${#files[@]} )) || return 0
+
   # skip first and last arg
   for arg in ${@:2:# - 2}; do
-    (( ${#files[@]} )) || return 0
-
     files=(`rg --files-with-matches ${arg} ${files[@]}`)
+
+    (( ${#files[@]} )) || return 0
   done
 
   rg ${lastArg} ${files[@]}
