137497703cbff9959f7df0da84b45a0acdc2b1a2
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-03-31 15:32:35 -0300

    support video in v command

--- src/spells/files/v.zsh
+++ src/spells/files/v.zsh
@@ -1,9 +1,11 @@
 if (( $ZSB_MACOS )); then
   ${zsb}.v.openCsv() open "${1}"
   ${zsb}.v.openImg() open "${1}"
+  ${zsb}.v.openVideo() open "${1}"
 else
   ${zsb}.v.openCsv() libreoffice --calc "${1}"
   ${zsb}.v.openImg() sxiv -f "${1}"
+  ${zsb}.v.openVideo() vlc "${1}"
 fi
 
 v() {
@@ -11,6 +13,8 @@ v() {
 
   case "$file" in
     *.xlsx|*.csv|*.odt) ${zsb}.v.openCsv "$file" ;;
+
+    *.mp4|*.mkv|*.avi|*.mov|*.webm|*.flv|*.wmv) ${zsb}.v.openVideo "$file" ;;
 
     *.pdf \
     |*.jpg \
