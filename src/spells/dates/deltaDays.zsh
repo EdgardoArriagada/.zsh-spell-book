adf7d072572b30001f8586d55bb0f655cef1d921
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-04-11 10:46:30 -0400

    update delta days

--- src/spells/dates/deltaDays.zsh
+++ src/spells/dates/deltaDays.zsh
@@ -1,8 +1,9 @@
+# input in YYYY-MM-DD format
+# example: `deltaDays 2022-12-03 2022-12-09`
+
 deltaDays() {
-  # input in YYYY-MM-DD format
-  # count 2022-12-03 2022-12-09
-  date1=$(date -j -f "%Y-%m-%d"  "$1" +%s)
-  date2=$(date -j -f "%Y-%m-%d"  "$2" +%s)
+  local date1=`date -j -f '%Y-%m-%d'  ${1} +%s`
+  local date2=`date -j -f '%Y-%m-%d'  ${2} +%s`
 
   printf $(( (date2-date1)/86400 ))
 }
