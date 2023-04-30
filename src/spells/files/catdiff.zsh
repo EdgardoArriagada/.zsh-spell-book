46e7fcdc184db76bc3b4fef8e99ea5f283c9feb1
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-04-29 20:15:56 -0400

    refactor catdiff

--- src/spells/files/catdiff.zsh
+++ src/spells/files/catdiff.zsh
@@ -3,13 +3,23 @@ catdiff() (
   local file1="$1"
   local file2="$2"
 
-  ${this}.main() {
-    if ${this}.areArgsInvalid; then
-      ${this}.throwArgsInvalidException; return $?
+  ${this}.assertValidArgs() {
+    if [[ ! -f ${file1} || ! -f ${file2} ]]
+      then ${zsb}.throw "Two valid files to compare expected"
     fi
+  }
 
-    local file1Lacks=$(${this}.compareFiles "$file1" "$file2")
-    local file2Lacks=$(${this}.compareFiles "$file2" "$file1")
+  ${this}.printFileInfo() {
+    ${zsb}.info "`hl ${1}` lacks of the following lines:\n"
+  }
+
+  ${this}.compareFiles() comm -13 <(sort $1) <(sort $2)
+
+  { # main
+    ${this}.assertValidArgs
+
+    local file1Lacks=`${this}.compareFiles ${file1} ${file2}`
+    local file2Lacks=`${this}.compareFiles ${file2} ${file1}`
 
     if [[ -z "$file1Lacks" && -z "$file2Lacks" ]]; then
       ${zsb}.info "Both files are equal"
@@ -17,35 +27,20 @@ catdiff() (
     fi
 
     if [[ -n "$file1Lacks" ]]; then
-      ${this}.printFileInfo "$file1"
-      echo "$file1Lacks"
+      ${this}.printFileInfo ${file1}
+      print ${file1Lacks}
     fi
 
     if [[ -n "$file1Lacks" && -n "$file2Lacks" ]]; then
       hr
-      echo " "
+      print ""
     fi
 
     if [[ -n "$file2Lacks" ]]; then
-      ${this}.printFileInfo "$file2"
-      echo "$file2Lacks"
+      ${this}.printFileInfo ${file2}
+      printf ${file2Lacks}
     fi
 
-    echo " "
+    print ""
   }
-
-  ${this}.areArgsInvalid() [[ ! -f "$file1" || ! -f "$file2" ]]
-
-  ${this}.throwArgsInvalidException() {
-    ${zsb}.throw "Two valid files to compare expected"
-  }
-
-  ${this}.printFileInfo() {
-    ${zsb}.info "`hl ${1}` lacks of the following lines:"
-    echo " "
-  }
-
-  ${this}.compareFiles() comm -13 <(sort $1) <(sort $2)
-
-  ${this}.main "$@"
 )
