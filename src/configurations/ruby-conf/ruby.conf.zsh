79872fc6c605d21d577f68457a09e6a6a1ea40af
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-05-04 11:45:17 -0400

    update ruby conf

--- src/configurations/ruby-conf/ruby.conf.zsh
+++ src/configurations/ruby-conf/ruby.conf.zsh
@@ -1,9 +1,9 @@
 # rbenv ubuntu https://github.com/rbenv/rbenv
-if [[ -d "$HOME/.rbenv" ]]; then
-  __${zsb}.prepareLazyLoad "$ZSB_DIR/src/configurations/ruby-conf/rbenv.init" \
+if [[ -d ${HOME}/.rbenv ]]; then
+  ${zsb}.prepareLazyLoad ${ZSB_DIR}/src/configurations/ruby-conf/rbenv.init \
     rbenv ruby gem irb bundle rspec yard solargraph
 
-  __${zsb}.prepareLazyLoad "$ZSB_DIR/src/configurations/ruby-conf/rails.init" \
+  ${zsb}.prepareLazyLoad ${ZSB_DIR}/src/configurations/ruby-conf/rails.init \
     rails rake
 fi
 
