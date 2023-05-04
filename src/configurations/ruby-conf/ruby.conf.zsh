9b1f88f9fe09e534e8157f5d211d955dd3fc6731
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-05-04 14:31:03 -0400

    boy scout

--- src/configurations/ruby-conf/ruby.conf.zsh
+++ src/configurations/ruby-conf/ruby.conf.zsh
@@ -2,10 +2,12 @@
 if [[ -d ${HOME}/.rbenv ]]; then
   ZSB_RUBY_CONF_DIR=${ZSB_DIR}/src/configurations/ruby-conf
 
-  ${zsb}.prepareLazyLoad ${ZSB_RUBY_CONF_DIR}/rbenv.init \
+  ${zsb}.prepareLazyLoad \
+    ${ZSB_RUBY_CONF_DIR}/rbenv.init \
     rbenv ruby gem irb bundle rspec yard solargraph
 
-  ${zsb}.prepareLazyLoad ${ZSB_RUBY_CONF_DIR}/rails.init \
+  ${zsb}.prepareLazyLoad \
+    ${ZSB_RUBY_CONF_DIR}/rails.init \
     rails rake
 fi
 
