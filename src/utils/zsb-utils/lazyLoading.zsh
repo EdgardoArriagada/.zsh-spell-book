50c5455bc257c3694f41c3363c70d6b0f8f27b00
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-05-04 11:12:16 -0400

    add back lazyLoading

---
+++ src/utils/zsb-utils/lazyLoading.zsh
@@ -0,0 +1,24 @@
+# usefull link https://gist.github.com/QinMing/364774610afc0e06cc223b467abe83c0#file-zshrc-L15-L37
+# thanks to QinMing for the code!
+${zsb}.lazyLoad() {
+  local aliasesToRelease=("${(@s: :)${1}}") # space separated list
+  local scriptToLazyLoad=$2
+  local commandToRun=$3
+  shift 3
+  local commandToRunArgs=$*
+
+  ${zsb}.info "Lazy loading..."
+
+  unalias ${aliasesToRelease[@]}
+  ${scriptToLazyLoad}
+  eval "${commandToRun} ${commandToRunArgs}"
+}
+
+${zsb}.prepareLazyLoad() {
+  local -r script=$1
+  shift 1
+
+  for cmd in ${@}; do
+    alias ${cmd}="${zsb}.lazyLoad \"${*}\" ${script} ${cmd}"
+  done
+}
