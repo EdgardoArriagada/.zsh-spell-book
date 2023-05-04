4624357e1c611642773b6c0aa60ae6e8d9fef070
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-05-04 12:08:45 -0400

    make compleions for rbenv

---
+++ src/configurations/ruby-conf/rbenv.completion.zsh
@@ -0,0 +1,16 @@
+# list generated with rbenv -v
+compdef "_${zsb}.nonRepeatedListD \
+   'commands:List all available rbenv commands' \
+   'local:Set or show the local application-specific Ruby version' \
+   'global:Set or show the global Ruby version' \
+   'shell:Set or show the shell-specific Ruby version' \
+   'install:Install a Ruby version using ruby-build' \
+   'uninstall:Uninstall a specific Ruby version' \
+   'rehash:Rehash rbenv shims (run this after installing executables)' \
+   'version:Show the current Ruby version and its origin' \
+   'versions:List installed Ruby versions' \
+   'which:Display the full path to an executable' \
+   'whence:List all Ruby versions that contain the given executable' \
+  " rbenv
+
+
