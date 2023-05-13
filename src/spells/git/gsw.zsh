fe63cf0ada7f71295bdc34a441c2a2f1c732581a
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-05-13 00:24:53 -0400

    boy scout

--- src/spells/git/gsw.zsh
+++ src/spells/git/gsw.zsh
@@ -10,8 +10,8 @@ hisIgnore 'gsw -' 'gsw 0' 'gsw ='
 _${zsb}.gsw() {
   [[ "$CURRENT" -gt "2" ]] && return 0
 
-  local localBranhces=( $(${zsb}.gitBranches) )
-  local currentBranch=( $(${zsb}.gitBranches 'current') )
+  local localBranhces=( `${zsb}.gitBranches` )
+  local currentBranch=( `${zsb}.gitBranches 'current'` )
 
   local newCompletion=( ${localBranhces:|currentBranch} )
   _describe 'command' newCompletion
