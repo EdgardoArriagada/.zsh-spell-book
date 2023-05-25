f3d661afac2178a97655b185aac53f6d47c451d3
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-05-24 20:28:56 -0400

    rename gitSwitchDefault

--- src/spells/git/gitSwitchDefault.zsh
+++ src/spells/git/gitSwitchDefault.zsh
@@ -1,14 +1,14 @@
-${zsb}.gitSwitch() {
+${zsb}.gitSwitchDefault() {
   local branch=`${zsb}.gitBranches | rg -m1 "^${1}$"`
 
-  if [[ -n "${branch}" ]]
+  if [[ -n "$branch" ]]
     then git switch ${branch}
     else ${zsb}.throw "No branch found for ${1}"
   fi
 }
 
-alias gswm="${zsb}.gitSwitch '${ZSB_GIT_MAIN_BRANCHES}'"
-alias gswd="${zsb}.gitSwitch '${ZSB_GIT_DEVELOP_BRANCHES}'"
+alias gswm="${zsb}.gitSwitchDefault '${ZSB_GIT_MAIN_BRANCHES}'"
+alias gswd="${zsb}.gitSwitchDefault '${ZSB_GIT_DEVELOP_BRANCHES}'"
 
 hisIgnore gswm gswd
 
