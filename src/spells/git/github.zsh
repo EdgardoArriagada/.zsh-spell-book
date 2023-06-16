1005a9f98ecd77df7c1d8c8f85ba41134685556f
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-06-15 20:56:51 -0400

    add some early returns

--- src/spells/git/github.zsh
+++ src/spells/git/github.zsh
@@ -1,7 +1,13 @@
 github() {
   zparseopts -D -E -F -- -compare=compare  || return 1
 
+  ${zsb}.validateGitRepo
+
   local url=`get_repo_url`
+
+  if [[ -z "$url" ]]; then
+    ${zsb}.throw "Remote url not set."
+  fi
 
   local tail
   [[ -n "$compare" ]] && tail="/compare/`git branch --show-current`"
