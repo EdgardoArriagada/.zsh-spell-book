4e589b156e53872a32a3c8faab00a3c1b7a02784
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-06-23 23:33:42 -0400

    make alert a script instead

--- src/utils/alert.zsh
+++ src/utils/alert.zsh
@@ -1,14 +1,3 @@
-alert() {
-  zparseopts -D -E -F -- i:=_icon || return 1
-  local icon="${_icon[2]}"
-  : ${icon:='timer'}
-
-  if (( $ZSB_MACOS ))
-    then osascript -e "tell app \"System Events\" to display dialog \"${*}\"" >/dev/null 2>&1
-    else notify-send --icon "$icon" --urgency critical "$*"
-  fi
-}
-
 alias a="alert"
 alias ad="alert 'DONE'"
 
