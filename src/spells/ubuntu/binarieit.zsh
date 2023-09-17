bcd320363e3343e49f882510792e8b80c27c45b7
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-09-17 16:24:48 -0300

    improve binarieit

--- src/spells/ubuntu/binarieit.zsh
+++ src/spells/ubuntu/binarieit.zsh
@@ -1,6 +1,8 @@
 binarieit() {
   local executable=${1:?'You must provide an executable file'}
 
-  sudo ln -s $(pwd)/${1} /usr/local/bin/ &&
-    ${zsb}.success "/usr/local/bin/$(hl "$1")"
+  local sourceBin=$(pwd)/${executable}
+
+  sudo ln -s $(pwd)/${executable} /usr/local/bin/ &&
+    ${zsb}.success "/usr/local/bin/$(hl "$1") -> $(pwdd)/$(hl ${executable})"
 }
