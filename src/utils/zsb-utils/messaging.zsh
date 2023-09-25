d0e58d908dcf357241628750792b9b74282b3539
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-09-25 11:28:45 -0300

    add skip and boy scout

--- src/utils/zsb-utils/messaging.zsh
+++ src/utils/zsb-utils/messaging.zsh
@@ -3,30 +3,32 @@ ZSB_MESSAGE_BADGE=`color 245 zsb`
 ${zsb}.message() <<< "${ZSB_MESSAGE_BADGE} `color ${1} ${2}`: ${@:3}"
 
 ${zsb}.throw() {
-  ${zsb}.message $ZSB_RED 'ERROR' "$@"
+  ${zsb}.message $ZSB_RED ERROR $@
   throw Error
 }
 
 ${zsb}.cancel() {
-  ${zsb}.message $ZSB_CYAN 'CANCEL' "$@"
+  ${zsb}.message $ZSB_CYAN CANCEL $@
   throw Error
 }
 
-${zsb}.success() ${zsb}.message $ZSB_GREEN 'SUCCESS' "$@"
+${zsb}.skip() ${zsb}.message $ZSB_YELLOW SKIP $@
 
-${zsb}.fail() ${zsb}.message $ZSB_RED 'FAIL' "$@"
+${zsb}.success() ${zsb}.message $ZSB_GREEN SUCCESS $@
 
-${zsb}.pass() ${zsb}.message $ZSB_GREEN 'PASS' "$@"
+${zsb}.fail() ${zsb}.message $ZSB_RED FAIL $@
 
-${zsb}.warning() ${zsb}.message $ZSB_YELLOW 'WARNING' "$@"
+${zsb}.pass() ${zsb}.message $ZSB_GREEN PASS $@
 
-${zsb}.info() ${zsb}.message $ZSB_BLUE 'INFO' "$@"
+${zsb}.warning() ${zsb}.message $ZSB_YELLOW WARNING $@
 
-${zsb}.prompt() ${zsb}.message $ZSB_PURPLE 'PROMPT' "$@"
+${zsb}.info() ${zsb}.message $ZSB_BLUE INFO $@
 
-${zsb}.expected() ${zsb}.message $ZSB_GREEN '+ Expected' "$@"
+${zsb}.prompt() ${zsb}.message $ZSB_PURPLE PROMPT $@
 
-${zsb}.current() ${zsb}.message $ZSB_RED '- Current' "$@"
+${zsb}.expected() ${zsb}.message $ZSB_GREEN '+ Expected' $@
+
+${zsb}.current() ${zsb}.message $ZSB_RED '- Current' $@
 
 ${zsb}.validate() {
   [[ -z "${(P)1}" ]] && ${zsb}.throw "You must set `hl ${1}` first."
