87d5a000ee06d5937e6c61139a310c30fb65cc12
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-09-25 11:29:49 -0300

    add skip and boy scout

--- src/spells/docker/removeStoppedContainers.zsh
+++ src/spells/docker/removeStoppedContainers.zsh
@@ -2,13 +2,13 @@ removeStoppedContainers() {
   local stoppedContainers=`docker container ls -aq`
 
   [[ -z "$stoppedContainers" ]] &&
-    ${zsb}.info "There are no stopped containers to remove." &&
+    ${zsb}.skip 'There are no stopped containers to remove.' &&
     return 0
 
-  ${zsb}.info "Removing all stopped Containers..."
+  ${zsb}.info 'Removing all stopped Containers...'
 
   docker container rm `printf ${stoppedContainers}` && \
-    ${zsb}.success "All containers were removed."
+    ${zsb}.success 'All containers were removed.'
 }
 
 _${zsb}.nocompletion removeStoppedContainers
