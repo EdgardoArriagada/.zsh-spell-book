6a13c2ff02107df6cb5c86638d140909e4cfb0b5
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-10-10 21:00:41 -0300

    wip modify pomodoro

--- src/spells/pomodoro/pomodoro.zsh
+++ src/spells/pomodoro/pomodoro.zsh
@@ -1,23 +1,11 @@
 # send instructions to pomodoro tmux session without attaching to it
 pomodoro() (
   local this=$0
-  local POMODORO_REGEX='^⏳'
-
-  local totalSeconds
   local inputTime=$1
   shift 1
   local pomodoroLabel=${*}
 
-  ${this}.createPomodoroTmuxSession() {
-    if ! tmux has-session -t pomodoro 2>/dev/null
-      then tmux new -d -s pomodoro
-    fi
-  }
-
-  ${this}.isPomodoroRunning() {
-    local lastPomodoroLines=$(tmux capture-pane -p -t pomodoro | sed '/^$/d' | tail -1)
-    ${zsb}.doesMatch "$lastPomodoroLines" "$POMODORO_REGEX"
-  }
+  local cbMessage
 
   ${this}.validatePomodoroNotRunning() {
     if ${this}.isPomodoroRunning; then
@@ -25,31 +13,26 @@ pomodoro() (
     fi
   }
 
-  ${this}.clearPomodoroSessionTty() tmux send-keys -t pomodoro.0 C-g
+
+  ${this}.setBeginString() {
+    if [[ -z "$label" ]]
+      then cbMessage="Session for ${inputTime}"
+      else cbMessage="${label} for ${inputTime}"
+    fi
+  }
 
   ${this}.beginPomodoro() {
-    # the white space at the beginning is to
-    # skip it from being saved to zsh history
-    local pomodoroCmd="
-      ${zsb}.pomodoro.startPomodoro ${totalSeconds} ${inputTime} '${pomodoroLabel}'
-      tmux send-keys -R \; clear-history
-    "
-    tmux send-keys -t pomodoro.0 ${pomodoroCmd} ENTER
+    pdoro -t $inputTime -c "pdoro_callback --phase started ${cbMessage}"
   }
 
   { # main
-    totalSeconds=`${zsb}.pomodoro.convertToSeconds ${inputTime}`
+    ## TODO: validate input time
 
-    ${zsb}.pomodoro.validateSeconds ${totalSeconds}
+    ## TODO: validate pomodoro not running
 
-    ${this}.createPomodoroTmuxSession
-
-    ${this}.validatePomodoroNotRunning
-
-    ${this}.clearPomodoroSessionTty
 
     ${this}.beginPomodoro
 
-    ${zsb}.success 'Pomodoro started.'
+    # ${zsb}.success 'Pomodoro started.'
   }
 )
