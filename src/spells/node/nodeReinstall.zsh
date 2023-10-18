6eafd258d53a7584901775bba48a4864d6d6709c
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-10-18 14:17:31 -0300

    support clean install

--- src/spells/node/nodeReinstall.zsh
+++ src/spells/node/nodeReinstall.zsh
@@ -2,7 +2,7 @@ ${zsb}.nodeReinstall() {
   local packageManager=$1
   local packageManagerLock=$2
 
-  zparseopts -D -E -F -- l=keepLockFile n=keepNodeModules i=skipInstall || return 1
+  zparseopts -D -E -F -- l=keepLockFile n=keepNodeModules i=skipInstall c=cacheClean || return 1
 
   if [[ -z "$keepLockFile" && -f ${packageManagerLock} ]]
     then messageStatus rm ${packageManagerLock}
@@ -10,6 +10,10 @@ ${zsb}.nodeReinstall() {
 
   if [[ -z "$keepNodeModules" && -d node_modules ]]
     then spinner rm -rf node_modules
+  fi
+
+  if [[ -n "$cacheClean" ]]
+    then npm cache clean --force
   fi
 
   if [[ -n "$skipInstall" ]]
@@ -21,7 +25,7 @@ ${zsb}.nodeReinstall() {
   alert "🏁 ${packageManager} install 🏁"
 }
 
-compdef "_${zsb}.nonRepeatedListD '-l:Keep lock files' '-n:Keep node modules' '-i:Skip installation'" ${zsb}.nodeReinstall
+compdef "_${zsb}.nonRepeatedListD '-l:Keep lock files' '-n:Keep node modules' '-i:Skip installation' '-c:Cache clean'" ${zsb}.nodeReinstall
 
 alias npmreinstall="${zsb}.nodeReinstall npm package-lock.json"
 
