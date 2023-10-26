7bf43fecff51db673848f491b7f1fac827cf2e90
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-10-26 11:21:48 -0300

    create version dic

---
+++ src/spells/ubuntu/version.zsh
@@ -0,0 +1,18 @@
+declare -gAr ZSH_VERSION_DIC=(
+  [tmux]='tmux -V'
+  [choose]='choose -v'
+  [rust]='hasRust'
+  [go]='hasGo'
+)
+
+version() {
+  local input=${1?Error: no input given}
+  local cmd=${ZSH_VERSION_DIC[$input]}
+
+  if [[ -n "$cmd" ]]
+    then printAndRun "$cmd"
+    else printAndRun "$input --version"
+  fi
+}
+
+compdef "_${zsb}.singleCompC 'compgen -c'" version
