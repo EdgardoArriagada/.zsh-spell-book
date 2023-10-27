966c4860830c3d5f68b71bfa730b559a6ae6e03f
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-10-27 13:04:37 -0300

    boy scout

--- src/utils/git-utils/branchValidators.zsh
+++ src/utils/git-utils/branchValidators.zsh
@@ -1,11 +1,11 @@
 ${zsb}.doesBranchesMatch() {
-  local inputBranch=${1}
-  local branchesRegex=${2}
-  ${zsb}.doesMatch ${inputBranch} "^${branchesRegex}$"
+  local inputBranch=$1
+  local branchesRegex=$2
+  ${zsb}.doesMatch $inputBranch "^${branchesRegex}$"
 }
 
-${zsb}.isDefaultBranch() ${zsb}.doesBranchesMatch ${1} ${ZSB_GIT_DEFAULT_BRANCHES}
-${zsb}.isMainBranch() ${zsb}.doesBranchesMatch ${1} ${ZSB_GIT_MAIN_BRANCHES}
+${zsb}.isDefaultBranch() ${zsb}.doesBranchesMatch $1 $ZSB_GIT_DEFAULT_BRANCHES
+${zsb}.isMainBranch() ${zsb}.doesBranchesMatch $1 $ZSB_GIT_MAIN_BRANCHES
 
 ${zsb}.userWorkingOnDefaultBranch() ${zsb}.isDefaultBranch `git branch --show-current`
 ${zsb}.userWorkingOnMainBranch() ${zsb}.isMainBranch `git branch --show-current`
