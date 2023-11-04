b4b811c3c4b503dcc719692f8242e0f9ff593a98
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-11-03 22:58:36 -0500

    add some usefull tmw shortcuts

--- src/spells/ubuntu/cdw.zsh
+++ src/spells/ubuntu/cdw.zsh
@@ -3,7 +3,12 @@ cdw2() cds ${ZSB_PROJECTS_DIR}2/$1
 cdw3() cds ${ZSB_PROJECTS_DIR}3/$1
 cdw4() cds ${ZSB_PROJECTS_DIR}4/$1
 
-compdef "_${zsb}.singleCompC 'ls $ZSB_PROJECTS_DIR'" cdw
-compdef "_${zsb}.singleCompC 'ls ${ZSB_PROJECTS_DIR}2'" cdw2
-compdef "_${zsb}.singleCompC 'ls ${ZSB_PROJECTS_DIR}3'" cdw3
-compdef "_${zsb}.singleCompC 'ls ${ZSB_PROJECTS_DIR}4'" cdw4
+tmw() { cd $ZSB_PROJECTS_DIR/$1 && tm $1 }
+tmw2() { cd ${ZSB_PROJECTS_DIR}2/$1 && tm $1 }
+tmw3() { cd ${ZSB_PROJECTS_DIR}3/$1 && tm $1 }
+tmw4() { cd ${ZSB_PROJECTS_DIR}4/$1 && tm $1 }
+
+compdef "_${zsb}.singleCompC 'ls $ZSB_PROJECTS_DIR'" cdw tmw
+compdef "_${zsb}.singleCompC 'ls ${ZSB_PROJECTS_DIR}2'" cdw2 tmw2
+compdef "_${zsb}.singleCompC 'ls ${ZSB_PROJECTS_DIR}3'" cdw3 tmw3
+compdef "_${zsb}.singleCompC 'ls ${ZSB_PROJECTS_DIR}4'" cdw4 tmw4
