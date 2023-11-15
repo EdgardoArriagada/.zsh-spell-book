4135237b3e8240e81507e74c3d3ce6788b281367
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-11-15 18:32:24 -0300

    prevent session collisions

--- src/spells/ubuntu/cdw.zsh
+++ src/spells/ubuntu/cdw.zsh
@@ -4,9 +4,9 @@ cdw3() cds ${ZSB_PROJECTS_DIR}3/$1
 cdw4() cds ${ZSB_PROJECTS_DIR}4/$1
 
 tmw() { tm $1 -c $ZSB_PROJECTS_DIR/$1 }
-tmw2() { tm $1 -c ${ZSB_PROJECTS_DIR}2/$1 }
-tmw3() { tm $1 -c ${ZSB_PROJECTS_DIR}3/$1 }
-tmw4() { tm $1 -c ${ZSB_PROJECTS_DIR}4/$1 }
+tmw2() { tm ${1}-2 -c ${ZSB_PROJECTS_DIR}2/$1 }
+tmw3() { tm ${1}-3 -c ${ZSB_PROJECTS_DIR}3/$1 }
+tmw4() { tm ${1}-4 -c ${ZSB_PROJECTS_DIR}4/$1 }
 
 compdef "_${zsb}.singleCompC 'ls $ZSB_PROJECTS_DIR'" cdw tmw
 compdef "_${zsb}.singleCompC 'ls ${ZSB_PROJECTS_DIR}2'" cdw2 tmw2
