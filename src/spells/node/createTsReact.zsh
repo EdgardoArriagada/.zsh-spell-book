895993f2c9724880bc1fb50a06f7fdde77646fa4
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2023-12-26 16:54:07 -0300

    make which type of app dynamic

--- src/spells/node/createTsReact.zsh
+++ src/spells/node/createTsReact.zsh
@@ -1,10 +1,12 @@
-createTsReact() {
+__createReactApp() {
+  local template=$1
+  shift
   local args=${@:?Error: You must provide a project name.}
   local projectName=`wcase -w $args --kebab`
 
   [[ -d $projectName ]] && ${zsb}.throw "$(hl "$1") is already a dir."
 
-  npm create vite@latest $projectName -- --template ts-react && \
+  npm create vite@latest $projectName -- --template $template && \
     cd $projectName && \
     prettierHelper --init && \
     alert -i 'face-cool' "Happy Hacking" && \
@@ -12,3 +14,6 @@ createTsReact() {
 
   alert -i 'face-sad' 'Something went wrong'
 }
+
+alias createTsReactApp='__createReactApp ts-react'
+alias createReactApp='__createReactApp react'
