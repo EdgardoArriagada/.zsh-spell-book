c358e5fca4f845065d2b244a9ff52ab290985c93
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-01-03 21:42:43 -0300

    improve dondedice to be able to search same term with multiple cases

--- src/spells/files/dondedice.zsh
+++ src/spells/files/dondedice.zsh
@@ -1,9 +1,27 @@
 # when $# > 1, $1 is a comma separated list of globs
-dondedice() {
+
+__dondedice() {
   local maxColumns=`${zsb}.getMaxSearchColumns`
-  case $# in
-    0) return 1 ;;
-    1) rg --glob '!{package-lock.json}' $1 -M $maxColumns ;;
-    *) rg --glob "!{package-lock.json,${1}}" "${@:2}" -M $maxColumns ;;
-  esac
+
+  if [[ -n "$skip" ]];then
+     rg --glob "!{package-lock.json,${skip[2]}}" $@ -M $maxColumns
+     return $?
+  fi
+
+  rg --glob '!{package-lock.json}' $@ -M $maxColumns
+}
+
+dondedice() {
+  zparseopts -D -E -F -- m=multiCase s:=skip
+
+  local searchInput=${1:?Error: Search input required.}
+  shift
+
+  if [[ -z "$multiCase" ]]
+    then __dondedice $searchInput $@ && return $?
+  fi
+
+  for kase in $ZSB_WCASE_CASES; do
+    __dondedice "`wcase --${kase} -w $searchInput`" $@
+  done
 }
