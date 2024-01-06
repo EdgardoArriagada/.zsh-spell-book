c6112b6eda4eb76bac0e178834ee40e346acf6f6
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-01-06 12:15:16 -0300

    add state to purge

--- src/spells/nvim/purgeNvim.zsh
+++ src/spells/nvim/purgeNvim.zsh
@@ -2,5 +2,6 @@ purgeNvim() {
   ${zsb}.confirmMenu.warning "You are about to purge nvim files"
 
   spinner rm -rf ~/.local/share/nvim
+  spinner rm -rf ~/.local/state/nvim
   spinner rm -rf ~/.cache/nvim
 }
