8e178144ef37860cd2ecdfeb38960b5d889009e8
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-03-02 15:33:28 -0300

    improve setReactNativeEnv

--- src/spells/react-native/setReactNativeEnv.zsh
+++ src/spells/react-native/setReactNativeEnv.zsh
@@ -1,4 +1,7 @@
 setReactNativeEnv() {
   export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
   java --version
+  export ANDROID_HOME=$HOME/Library/Android/sdk
+  export PATH=$PATH:$ANDROID_HOME/emulator
+  export PATH=$PATH:$ANDROID_HOME/platform-tools
 }
