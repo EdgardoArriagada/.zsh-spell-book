8e8847ca21f1dd60775ff6314736cb14bcbea722
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-03-26 09:18:01 -0300

    add gleam config files

--- src/globalVariables.zsh
+++ src/globalVariables.zsh
@@ -16,8 +16,8 @@ declare ZSB_GIT_DEVELOP_BRANCHES_ARRAY=(develop)
 ZSB_GIT_AWARE="--aware:Proceed even if affecting a default branch"
 ZSB_GIT_FORCE="--force:Proceed even in commit have already been pushed online"
 
-ZSB_GIT_LOCK_FILES=( package-lock.json pnpm-lock.yaml Gemfile.lock yarn.lock Cargo.lock go.sum )
-ZSB_GIT_PACKAGE_FILES=( package.json Gemfile Cargo.toml go.mod )
+ZSB_GIT_LOCK_FILES=( package-lock.json pnpm-lock.yaml Gemfile.lock yarn.lock Cargo.lock go.sum manifest.toml )
+ZSB_GIT_PACKAGE_FILES=( package.json Gemfile Cargo.toml go.mod gleam.toml )
 
 declare -gAr ZSB_GIT_FILETYPE_TO_REGEX=(
   ['staged']='^[MARCD]'
