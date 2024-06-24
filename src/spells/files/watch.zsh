ce3633d155cfc94bcdb6e8177799dec33a2e4753
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-06-24 13:50:01 -0400

    improve watch script

--- src/spells/files/watch.zsh
+++ src/spells/files/watch.zsh
@@ -1,5 +1,7 @@
 watch() {
-  local file=$1
+  local file=${1:?Error: you must provide a file to watch.}
+
+  [[ ! -a $file ]] && ${zsb}.throw "File not found: `hl $file`"
 
   case ${1:e:l} in
     lua) echo $file | entr -c lua $file ;;
