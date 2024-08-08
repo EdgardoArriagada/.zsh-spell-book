33bb88f085d2e95f0dcc74e3873cbbcedd7b8c17
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-08-07 22:44:37 -0400

    change to sh instead

--- src/spells/docker/dkssh.zsh
+++ src/spells/docker/dkssh.zsh
@@ -1,6 +1,6 @@
 dkssh() {
   local inputContainer=${1:?Error: You must provide a container.}
-  printAndRun "docker container exec -ti $inputContainer bash"
+  printAndRun "docker container exec -ti $inputContainer sh"
 }
 
 compdef "_${zsb}.singleCompC 'docker ps --format \"{{.Names}}\"'" dkssh
