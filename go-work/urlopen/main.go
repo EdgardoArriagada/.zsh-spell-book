2fcf7431cb2092067590a65b2b706f4aa89fa25c
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-10-19 14:59:53 -0300

    allow to receive args from pipe aswell

--- go-work/urlopen/main.go
+++ go-work/urlopen/main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"os/exec"
 	"runtime"
+	"strings"
 )
 
-func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: urlopen <url>")
-		os.Exit(1)
-	}
-
-	url := os.Args[1]
+func openURL(url string) error {
 	var cmd *exec.Cmd
 
 	switch runtime.GOOS {
@@ -25,9 +21,39 @@ func main() {
 		cmd = exec.Command("xdg-open", url)
 	}
 
-	err := cmd.Run()
-	if err != nil {
-		fmt.Printf("Error opening URL: %v\n", err)
-		os.Exit(1)
+	return cmd.Run()
+}
+
+func main() {
+	if len(os.Args) > 1 {
+		// If command-line arguments are provided, use them
+		for _, url := range os.Args[1:] {
+			if err := openURL(url); err != nil {
+				fmt.Printf("Error opening URL %s: %v\n", url, err)
+			}
+		}
+	} else {
+		// If no arguments, check if there's piped input
+		stat, _ := os.Stdin.Stat()
+		if (stat.Mode() & os.ModeCharDevice) == 0 {
+			// Data is being piped to stdin
+			scanner := bufio.NewScanner(os.Stdin)
+			for scanner.Scan() {
+				url := strings.TrimSpace(scanner.Text())
+				if url != "" {
+					if err := openURL(url); err != nil {
+						fmt.Printf("Error opening URL %s: %v\n", url, err)
+					}
+				}
+			}
+			if err := scanner.Err(); err != nil {
+				fmt.Printf("Error reading input: %v\n", err)
+			}
+		} else {
+			// No arguments and no piped input
+			fmt.Println("Usage: urlopen <url> [<url2> ...]")
+			fmt.Println("   or: echo <url> | urlopen")
+			os.Exit(1)
+		}
 	}
 }
