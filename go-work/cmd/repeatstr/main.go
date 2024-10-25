0ea35d59051bc1f394760ed3474c9575960354ed
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-10-25 14:33:08 -0300

    implement repeatstr

--- go-work/cmd/repeatstr/main.go
+++ go-work/cmd/repeatstr/main.go
@@ -2,17 +2,29 @@ package main
 
 import (
 	"example.com/workspace/lib/argsLib"
-	"example.com/workspace/lib/open"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	args, err := argsLib.Parse()
-	if err != nil || len(args) == 0 {
+	if err != nil || len(args) < 2 {
 		fmt.Println(err)
+		os.Exit(1)
+
+	}
+
+	n, err := strconv.Atoi(args[0])
+	if err != nil {
+		fmt.Println("First argument must be a number")
 		os.Exit(1)
 	}
 
-	open.Url(args[0]) // first args is url
+	word := args[1]
+	var result string
+	for i := 0; i < n; i++ {
+		result += word
+	}
+	fmt.Println(result)
 }
