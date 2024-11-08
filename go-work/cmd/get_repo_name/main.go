697731d74a6cb1a716271faa2fcf4a45f0c4f858
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-11-08 19:31:16 -0300

    move get repo name logic to a lib

--- go-work/cmd/get_repo_name/main.go
+++ go-work/cmd/get_repo_name/main.go
@@ -2,52 +2,15 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
+
+	"example.com/workspace/lib/git"
 )
 
 func main() {
-	pathToDotGit, err := getGitDir()
+	repo_name, err := git.GetRepoName()
 	if err != nil {
 		return
 	}
 
-	if pathToDotGit == ".git" {
-		fmt.Println(filepath.Base(getCurrentDir()))
-		return
-	}
-
-	tail := filepath.Base(pathToDotGit)
-	if tail != ".git" {
-		fmt.Println(tail)
-		return
-	}
-
-	parentRootDir := filepath.Dir(pathToDotGit)
-	fmt.Println(filepath.Base(parentRootDir))
-}
-
-// getCurrentDir returns the path of the current working directory.
-func getCurrentDir() string {
-	dir, err := os.Getwd()
-	if err != nil {
-		fmt.Println("Error getting the current directory:", err)
-		os.Exit(1)
-	}
-	return dir
-}
-
-// getGitDir returns the .git directory path of the current git repository.
-func getGitDir() (string, error) {
-	cmd := exec.Command("git", "rev-parse", "--git-dir")
-	cmd.Stderr = nil // silencing errors, similar to '2>/dev/null'
-
-	output, err := cmd.Output()
-	if err != nil {
-		return "", err
-	}
-
-	return strings.TrimSpace(string(output)), nil
+	fmt.Println(repo_name)
 }
--- go-work/lib/git/git.go
+++ go-work/lib/git/git.go
@@ -1,73 +1,52 @@
 package git
 
 import (
-	"bufio"
-	"errors"
-	"io"
+	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
-func isStdinEmpty() bool {
-	stat, _ := os.Stdin.Stat()
-	return (stat.Mode() & os.ModeCharDevice) != 0
+func GetRepoName() (string, error) {
+	pathToDotGit, err := getGitDir()
+	if err != nil {
+		return "", err
+	}
+
+	if pathToDotGit == ".git" {
+		return filepath.Base(getCurrentDir()), nil
+
+	}
+
+	tail := filepath.Base(pathToDotGit)
+	if tail != ".git" {
+		return tail, nil
+	}
+
+	parentRootDir := filepath.Dir(pathToDotGit)
+	return filepath.Base(parentRootDir), nil
 }
 
-func parseStdin() ([]string, error) {
-	var result []string
-
-	if isStdinEmpty() {
-		return result, nil
+// getCurrentDir returns the path of the current working directory.
+func getCurrentDir() string {
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error getting the current directory:", err)
+		os.Exit(1)
 	}
-
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		arg := scanner.Text()
-		if arg != "" {
-			result = append(result, arg)
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		return nil, errors.New("Error: Reading input")
-	}
-
-	return result, nil
+	return dir
 }
 
-func parseWholeStdin() (string, error) {
-	var result strings.Builder
+// getGitDir returns the .git directory path of the current git repository.
+func getGitDir() (string, error) {
+	cmd := exec.Command("git", "rev-parse", "--git-dir")
+	cmd.Stderr = nil // silencing errors, similar to '2>/dev/null'
 
-	if isStdinEmpty() {
-		return "", nil
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
 	}
 
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		line, err := reader.ReadString('\n')
-		if err != nil && err != io.EOF {
-			return "", errors.New("Error: Reading input")
-		}
-		result.WriteString(line)
-		if err == io.EOF {
-			break
-		}
-	}
-
-	return result.String(), nil
-}
-
-func Parse() ([]string, error) {
-	if len(os.Args) > 1 {
-		return os.Args[1:], nil
-	} else {
-		return parseStdin()
-	}
-}
-
-func ParseWhole() (string, error) {
-	if len(os.Args) > 1 {
-		return strings.Join(os.Args[1:], "\n"), nil
-	} else {
-		return parseWholeStdin()
-	}
+	return strings.TrimSpace(string(output)), nil
 }
