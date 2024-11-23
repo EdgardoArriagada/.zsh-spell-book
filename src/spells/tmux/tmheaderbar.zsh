28510c83e7fb63cd6790390c6020df943484e411
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-11-22 23:34:11 -0300

    handle special case where id is not given

--- src/spells/tmux/tmheaderbar.zsh
+++ src/spells/tmux/tmheaderbar.zsh
@@ -1,6 +1,9 @@
 tmheaderbar() {
-  local winId=$1
-  shift 1
+  local winId
+  if [[ $1 =~ ^@[0-9]+$ ]]
+    then winId=$1; shift 1
+    else winId=`tmux display -p "#{window_id}"`
+  fi
 
   tmux split-window -vb -p 6 -t ${winId}.0 -P -F "#{pane_id}" \; \
     send-keys -t ${winId}.0 " printCentered \"${*}\" | less; exit " Enter \; \
