dd540ebd4aaadbe10cb5be3f88c984b93d2ccce3
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-11-22 23:25:07 -0300

    one headbar o rule them all

--- src/spells/tmux/tmheaderbar.zsh
+++ src/spells/tmux/tmheaderbar.zsh
@@ -1,12 +1,4 @@
 tmheaderbar() {
-  tmux split-window -vb -p 6 -t 0 -P -F "#{pane_id}" \; \
-    send-keys -t 0 " printCentered \"${*}\" | less; exit " Enter \; \
-    select-pane -t 1
-}
-
-# param $1: sessionName=`tmux display-message -p '#S'`
-# param $2: windowNumber=`tmux display-message -p '#I'`
-tmheaderbarFixed() {
   local winId=$1
   shift 1
 
