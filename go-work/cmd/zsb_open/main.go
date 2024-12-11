bad5f8012d9c03e880727db212166e2b0ff930b4
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-12-11 19:02:13 -0300

    workaround

--- go-work/cmd/zsb_open/main.go
+++ go-work/cmd/zsb_open/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	"example.com/workspace/lib/args"
 	"example.com/workspace/lib/open"
 	u "example.com/workspace/lib/utils"
 )
 
 func main() {
+	fmt.Println("f") // prevent bug that erases the binary
 	d := u.Must(args.ParseWithStdin())
 	u.Expect(d.Len == 1, "Usage: zsb_open <url>")
 
-	open.Url(d.Args[0]) // first args is url
+	open.Url(d.Args[0])
 }
