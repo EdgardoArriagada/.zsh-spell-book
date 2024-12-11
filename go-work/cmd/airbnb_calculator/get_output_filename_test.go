4e817840a0b1885e3e417653efef6751697e5535
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-12-10 21:13:50 -0300

    split get_output_filename

--- go-work/cmd/airbnb_calculator/get_output_filename_test.go
+++ go-work/cmd/airbnb_calculator/get_output_filename_test.go
@@ -30,20 +30,3 @@ func TestGetOutputFilename(t *testing.T) {
 		}
 	}
 }
-
-func equal(a, b [][]string) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i := range a {
-		if len(a[i]) != len(b[i]) {
-			return false
-		}
-		for j := range a[i] {
-			if a[i][j] != b[i][j] {
-				return false
-			}
-		}
-	}
-	return true
-}
--- go-work/cmd/airbnb_calculator/process_records_test.go
+++ go-work/cmd/airbnb_calculator/process_records_test.go
@@ -79,3 +79,20 @@ func TestProcessRecords_MissingNochesColumn(t *testing.T) {
 		t.Errorf("Test failed")
 	}
 }
+
+func equal(a, b [][]string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if len(a[i]) != len(b[i]) {
+			return false
+		}
+		for j := range a[i] {
+			if a[i][j] != b[i][j] {
+				return false
+			}
+		}
+	}
+	return true
+}
