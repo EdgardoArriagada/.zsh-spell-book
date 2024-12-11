bebd6920a6dfd315bcd945e607c3e4394235fedd
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-12-10 22:43:02 -0300

    expand coverage

--- go-work/cmd/airbnb_calculator/calculate_iva_test.go
+++ go-work/cmd/airbnb_calculator/calculate_iva_test.go
@@ -5,14 +5,24 @@ import (
 )
 
 func TestCalculateIva_ShouldCalculate(t *testing.T) {
-	avaluo_fiscal := 83_231_614
-	days := 10
-	total_income := 300_000
+	tests := []struct {
+		avaluoFiscal int
+		days         int
+		totalIncome  int
+		expected     float64
+	}{
+		{83_231_614, 10, 300_000, 7_294},
+		{83_231_614, 15, 500_000, 18_924},
+		{56_612_271, 10, 300_000, 20_281},
+		{48_738_853, 3, 115716, 11_343},
+	}
 
-	expected := float64(7_294)
-	actual := CalculateIva(avaluo_fiscal, days, total_income)
-
-	if actual != expected {
-		t.Errorf("\nexpected: %f \nactual: %f", expected, actual)
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			actual := CalculateIva(tt.avaluoFiscal, tt.days, tt.totalIncome)
+			if actual != tt.expected {
+				t.Errorf("\nexpected: %f \nactual: %f", tt.expected, actual)
+			}
+		})
 	}
 }
