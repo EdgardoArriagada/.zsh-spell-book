833a72c0d78176207d6ca302ef9d9581b1bef78b
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-12-10 23:02:15 -0300

    improve api for better accuracy

--- go-work/cmd/airbnb_calculator/calculate_iva.go
+++ go-work/cmd/airbnb_calculator/calculate_iva.go
@@ -2,12 +2,39 @@ package main
 
 import "math"
 
-func CalculateIva(avaluo_fiscal int, days int, total_income int) float64 {
-	with_iva := 0.11 * float64(avaluo_fiscal) / (30 * 12) * float64(days)
+var daysPerMonth = map[int]int{
+	1:  31,
+	2:  28,
+	3:  31,
+	4:  30,
+	5:  31,
+	6:  30,
+	7:  31,
+	8:  31,
+	9:  30,
+	10: 31,
+	11: 30,
+	12: 31,
+}
+
+// make sure Feb has 29 days for leap years
+func getDaysInMonth(month int, year int) int {
+	if month == 2 && isLeapYear(year) {
+		return 29
+	}
+	return daysPerMonth[month]
+}
+
+func isLeapYear(year int) bool {
+	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
+}
+
+func CalculateIva(avaluo_fiscal int, total_income int, days int, month int, year int) float64 {
+	days_in_month := getDaysInMonth(month, year)
+	with_iva := 0.11 * float64(avaluo_fiscal) / (float64(days_in_month) * 12) * float64(days)
 	base_imponible := (float64(total_income) - with_iva) / 1.19
 
 	iva := base_imponible * 0.19
 
-	// ceil iva
 	return math.Ceil(iva)
 }
--- go-work/cmd/airbnb_calculator/calculate_iva_test.go
+++ go-work/cmd/airbnb_calculator/calculate_iva_test.go
@@ -7,19 +7,21 @@ import (
 func TestCalculateIva_ShouldCalculate(t *testing.T) {
 	tests := []struct {
 		avaluoFiscal int
-		days         int
 		totalIncome  int
+		days         int
+		month        int
+		year         int
 		expected     float64
 	}{
-		{83_231_614, 10, 300_000, 7_294},
-		{83_231_614, 15, 500_000, 18_924},
-		{56_612_271, 10, 300_000, 20_281},
-		{48_738_853, 3, 115716, 11_343},
+		{83_231_614, 300_000, 10, 11, 2024, 7_294},
+		{83_231_614, 500_000, 15, 11, 2024, 18_924},
+		{56_612_271, 300_000, 10, 1, 2024, 21_172},
+		{48_738_853, 115716, 3, 11, 2024, 11_343},
 	}
 
 	for _, tt := range tests {
 		t.Run("", func(t *testing.T) {
-			actual := CalculateIva(tt.avaluoFiscal, tt.days, tt.totalIncome)
+			actual := CalculateIva(tt.avaluoFiscal, tt.totalIncome, tt.days, tt.month, tt.year)
 			if actual != tt.expected {
 				t.Errorf("\nexpected: %f \nactual: %f", tt.expected, actual)
 			}
