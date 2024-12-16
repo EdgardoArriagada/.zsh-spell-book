94de47c23144e0d4e49296a5ad0bee82bdd00fe4
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-12-16 14:27:09 -0300

    add no ignore to normal search

--- src/spells/files/playMenuAndExecute.zsh
+++ src/spells/files/playMenuAndExecute.zsh
@@ -21,7 +21,7 @@ _${zsb}.playMenuAndExecute() {
   _describe 'command' newCompletion
 }
 
-__vg_dataGetter='fd -t f'
+__vg_dataGetter='fd -t f --no-ignore'
 __vg_command='v'
 __vg() ${zsb}.playMenuAndExecute $0 $__vg_command $__vg_dataGetter $@
 alias vg='noglob __vg'
@@ -35,7 +35,7 @@ alias vgh='noglob __vgh'
 compdef "_${zsb}.playMenuAndExecute $__vgh_dataGetter" __vgh
 hisIgnore vgh
 
-__tigg_dataGetter='fd -t f'
+__tigg_dataGetter='fd -t f --no-ignore'
 __tigg_command='tig'
 __tigg() ${zsb}.playMenuAndExecute $0 $__tigg_command $__tigg_dataGetter $@
 alias tigg='noglob __tigg'
@@ -49,7 +49,7 @@ alias tiggh='noglob __tiggh'
 compdef "_${zsb}.playMenuAndExecute $__tiggh_dataGetter" __tiggh
 hisIgnore tiggh
 
-__cg_dataGetter='fd -t f'
+__cg_dataGetter='fd -t f --no-ignore'
 __cg_command='c'
 __cg() ${zsb}.playMenuAndExecute $0 $__cg_command $__cg_dataGetter $@
 alias cg='noglob __cg'
@@ -62,7 +62,7 @@ __cgh() ${zsb}.playMenuAndExecute $0 $__cgh_command $__cgh_dataGetter $@
 alias cgh='noglob __cgh'
 hisIgnore cgh
 
-__cdg_dataGetter='fd -t d'
+__cdg_dataGetter='fd -t d --no-ignore'
 __cdg_command='cd'
 __cdg() ${zsb}.playMenuAndExecute $0 $__cdg_command $__cdg_dataGetter $@
 alias cdg='noglob __cdg'
@@ -76,7 +76,7 @@ alias cdgh='noglob __cdgh'
 compdef "_${zsb}.playMenuAndExecute $__cdgh_dataGetter" __cdgh
 hisIgnore cdgh
 
-__cdp_dataGetter='fd'
+__cdp_dataGetter='fd --no-ignore'
 __cdp_command='cd'
 __cdp() {
   if ! [[ -a "$1" ]]
