9808ce27c6de6ca54d6bb203b27bec615106ac5e
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2025-01-01 13:30:18 -0300

    merge dkattach

--- src/spells/docker/dkattach.zsh
+++ src/spells/docker/dkattach.zsh
@@ -1,16 +1,26 @@
-dkattach() {
+selectContainer() {
   local inputContainer
   if [[ -z $1 ]]; then
     local runningContainers=$(docker ps --format "{{.Names}}")
-    [[ -z $runningContainers ]] && ${zsb}.throw "No running containers found."
+    [[ -z $runningContainers ]] && return 1
 
     inputContainer=$(fzf <<< $runningContainers)
   else
     inputContainer=$1
   fi
+  <<< $inputContainer
+}
 
+dkattach() {
+  local inputContainer=$(selectContainer $1)
+  [[ -z "$inputContainer" ]] && ${zsb}.throw "No container"
   printAndRun "docker container attach $inputContainer"
 }
 
-compdef "_${zsb}.singleCompC 'docker ps --format \"{{.Names}}\"'" dkattach
+dkssh() {
+  local inputContainer=$(selectContainer $1)
+  [[ -z "$inputContainer" ]] && ${zsb}.throw "No container"
+  printAndRun "docker container exec -ti $inputContainer sh"
+}
 
+compdef "_${zsb}.singleCompC 'docker ps --format \"{{.Names}}\"'" dkattach dkssh
