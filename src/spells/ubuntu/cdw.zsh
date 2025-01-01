d40379d016946717d11c38d639536996caff3212
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2025-01-01 11:56:34 -0300

    make cdw usage simpler

--- src/spells/ubuntu/cdw.zsh
+++ src/spells/ubuntu/cdw.zsh
@@ -1,12 +1,33 @@
-cdw() cds $ZSB_PROJECTS_DIR/$1
-cdw2() cds ${ZSB_PROJECTS_DIR}2/$1
-cdw3() cds ${ZSB_PROJECTS_DIR}3/$1
-cdw4() cds ${ZSB_PROJECTS_DIR}4/$1
+${zsb}.cdw_generic() {
+    local project_dir=$1
+    if [[ -z "$2" ]]; then
+        local dir=$(ls $project_dir | fzf)
+        [[ -n "$dir" ]] && cds $project_dir/$dir
+    else
+        cds $project_dir/$2
+    fi
+}
 
-tmw() { tm $1 -c $ZSB_PROJECTS_DIR/$1 }
-tmw2() { tm ${1}-2 -c ${ZSB_PROJECTS_DIR}2/$1 }
-tmw3() { tm ${1}-3 -c ${ZSB_PROJECTS_DIR}3/$1 }
-tmw4() { tm ${1}-4 -c ${ZSB_PROJECTS_DIR}4/$1 }
+${zsb}.tmw_generic() {
+    local project_dir=$1
+    local suffix=$2
+    if [[ -z "$3" ]]; then
+        local dir=$(ls $project_dir | fzf)
+        [[ -n "$dir" ]] && tm ${dir}${suffix} -c $project_dir/$dir
+    else
+        tm ${3}${suffix} -c $project_dir/$3
+    fi
+}
+
+cdw() { ${zsb}.cdw_generic $ZSB_PROJECTS_DIR $1 }
+cdw2() { ${zsb}.cdw_generic ${ZSB_PROJECTS_DIR}2 $1 }
+cdw3() { ${zsb}.cdw_generic ${ZSB_PROJECTS_DIR}3 $1 }
+cdw4() { ${zsb}.cdw_generic ${ZSB_PROJECTS_DIR}4 $1 }
+
+tmw() { ${zsb}.tmw_generic $ZSB_PROJECTS_DIR "" $1 }
+tmw2() { ${zsb}.tmw_generic ${ZSB_PROJECTS_DIR}2 "-2" $1 }
+tmw3() { ${zsb}.tmw_generic ${ZSB_PROJECTS_DIR}3 "-3" $1 }
+tmw4() { ${zsb}.tmw_generic ${ZSB_PROJECTS_DIR}4 "-4" $1 }
 
 compdef "_${zsb}.singleCompC 'ls $ZSB_PROJECTS_DIR'" cdw tmw
 compdef "_${zsb}.singleCompC 'ls ${ZSB_PROJECTS_DIR}2'" cdw2 tmw2
