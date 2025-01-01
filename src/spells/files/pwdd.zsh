d9e55b8bc52a29c3a3d7b4a2a68df398a146c80c
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2025-01-01 12:54:01 -0300

    simplify pwdd

--- src/spells/files/pwdd.zsh
+++ src/spells/files/pwdd.zsh
@@ -1,21 +1,14 @@
 pwdd() {
   local result
 
-  zparseopts -D -E -F -- -copy=copy || return 1
-
   if [[ -n "$1" ]]
     then result="`print -P %~`/$1"
     else result="`print -P %~`"
   fi
 
-  if [[ -n "$copy" ]]; then
-    zsb_clipcopy $result
-    ${zsb}.info "`hl $result` copied"
-  else
-    <<< $result
-  fi
+  zsb_clipcopy $result
+  ${zsb}.info "`hl $result` copied"
 }
 
 hisIgnore pwdd
 
-complete -W "--copy" pwdd
