fd3a1ec945432a4a29028e743de95e0a35c7f26e
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2025-01-02 13:20:11 -0300

    improve docker blah

--- src/spells/docker/selectContainerAndExec.zsh
+++ src/spells/docker/selectContainerAndExec.zsh
@@ -1,22 +1,23 @@
 ${zsb}.selectContainerAndExecute() {
-  local rawCmd=$1
-  shift
+  local invoker=$1
+  local rawCmd=$2
+  shift 2
 
-  local inputContainer
-  if [[ -z "$1" ]]; then
+  local inputContainer=$1
+  if [[ -z "$inputContainer" ]]; then
     local runningContainers=$(docker ps --format "{{.Names}}")
     [[ -z "$runningContainers" ]] && ${zsb}.throw "No containers found"
 
     inputContainer=$(fzf <<< $runningContainers)
-  else
-    inputContainer=$1
+    print -z "$invoker $inputContainer"
+    return 0
   fi
 
   local cmd=$(echo $rawCmd | sed "s/{inputContainer}/$inputContainer/g")
   printAndRun $cmd
 }
 
-dkattach() ${zsb}.selectContainerAndExecute 'docker container attach {inputContainer}' $1
-dkssh() ${zsb}.selectContainerAndExecute 'docker container exec -ti {inputContainer} sh' $1
+dkattach() ${zsb}.selectContainerAndExecute $0 'docker container attach {inputContainer}' $1
+dkssh() ${zsb}.selectContainerAndExecute $0 'docker container exec -ti {inputContainer} sh' $1
 
 compdef "_${zsb}.singleCompC 'docker ps --format \"{{.Names}}\"'" dkattach dkssh
