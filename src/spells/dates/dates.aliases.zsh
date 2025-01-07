1c0fdd542fbe683c89c0114a0fac4c1b5ce7b72c
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2025-01-07 16:44:06 -0300

    normalize date

--- src/spells/dates/dates.aliases.zsh
+++ src/spells/dates/dates.aliases.zsh
@@ -1,1 +1,1 @@
-alias cpdate="zsb_clipcopy \`date +%d-%m-%Y\` && ${zsb}.success \"\`hl $(date +%d-%m-%Y)\` copied to clipboard.\""
+alias cpdate="zsb_clipcopy \`date +%Y-%m-%d\` && ${zsb}.success \"\`hl $(date +%Y-%m-%d)\` copied to clipboard.\""
