1b5df53fcb52b8424c03f8a15538a66c99679b1d
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2025-01-26 17:39:11 -0300

    make script more fiendlty

--- src/spells/git/changeFromHttpsToShh.zsh
+++ src/spells/git/changeFromHttpsToShh.zsh
@@ -6,5 +6,5 @@ changeFromHttpsToShh() {
     ${zsb}.throw "Could not fetch reponame or username"
   fi
 
-  git remote set-url origin git@github.com:$username/$reponame
+  printAndRun "git remote set-url origin git@github.com:$username/$reponame"
 }
