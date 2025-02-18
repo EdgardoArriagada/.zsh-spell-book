2d4bad565b44025f935aa2ddba33d9e5f0b31ad5
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2025-02-18 13:48:26 -0300

    review renametab

--- go-work/cmd/zsb_charm_tmux_renametab/main.go
+++ go-work/cmd/zsb_charm_tmux_renametab/main.go
@@ -14,13 +14,14 @@ import (
 
 func main() {
 	d := u.Must1(args.Parse())
-	u.Expect(d.Len == 1 || d.Len == 2, "Usage: zsb_charm_tmux_renametab <win_id> <current_file_path>")
+	u.Expect(d.Len == 2, "Usage: zsb_charm_tmux_renametab <win_id> <current_file_path>")
 
 	winId := d.Args[0]
-	currentFilePath := d.Get(1)
+	currentFilePath := d.Args[1]
 
 	repoRoot := u.Must1(git.GetRepoRoot())
 
+	// if a file is given when opening neovim, make sure it is in the repo
 	if currentFilePath != "" && !strings.HasPrefix(currentFilePath, repoRoot) {
 		os.Exit(1)
 	}
