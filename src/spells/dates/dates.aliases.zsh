29bf6ed7c9f8693c1eb627b92b3efaad9ce0d4eb
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2025-03-23 13:01:43 -0300

    make cpdate more user friendly

--- src/spells/dates/dates.aliases.zsh
+++ src/spells/dates/dates.aliases.zsh
@@ -1,5 +1,21 @@
 ${zsb}.cpdate_generic() { zsb_clipcopy $1 && ${zsb}.success "`hl $1` as `hl $2` copied to clipboard."; }
 
-alias cpdate="${zsb}.cpdate_generic `date +%Y-%m-%d` YYYY-MM-DD"
-alias cpdate2="${zsb}.cpdate_generic `date +%d-%m-%Y` DD-MM-YYYY"
+cpdate() {
+  zparseopts -D -E -F -- \
+    f1=format1 \
+    f2=format2 \
+    || return 1
 
+  if [[ -n "$format1" ]]; then
+    ${zsb}.cpdate_generic `date +%Y-%m-%d` YYYY-MM-DD
+  elif [[ -n "$format2" ]]; then
+    ${zsb}.cpdate_generic `date +%d-%m-%Y` DD-MM-YYYY
+  else
+    ${zsb}.cpdate_generic `date +%Y-%m-%d` YYYY-MM-DD
+  fi
+}
+
+compdef "_${zsb}.singleComp \
+  '-f1:YYYY-MM-DD' \
+  '-f2:DD-MM-YYYY' \
+  " cpdate
