9d13d2479985527fe5c21a56c867975fea16686b
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2025-04-21 16:09:34 -0400

    move expansion to its own function

--- src/spells/files/v.zsh
+++ src/spells/files/v.zsh
@@ -9,21 +9,6 @@ else
 fi
 
 ${zsb}.v() {
-  if [[ "$1" =~ '/' && ! -a $1 ]]; then
-    if ! ${zsb}.isGitRepo || [[ "$1" =~ '^(/|~|\.\.)' ]]
-      then ${zsb}.throw 'Unsafe to perform roman expansion.'
-    fi
-
-    if [[ "${1: -1}" == "/" ]]
-      then mkdir -p $1
-      else mkdir -p `dirname $1`
-    fi
-
-    nvim $1 && ${zsb}.tryGitStatus
-
-    return 0
-  fi
-
   case ${1:e:l} in
     xlsx|odt) ${0}.openCsv $1 ;;
 
