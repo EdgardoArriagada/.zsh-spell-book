8aca09cecd80a6677b224e943073694095a0d6ec
Edgardo Arriagada <edgardo.arriagada.ortiz@gmail.com>
2025-08-30 20:05:25 -0400

    Migrate bundler (#30)
    * llm changes
    
    * small tweak
    
    * fix no longer need to be in repo root to work
    
    * remove unused files
    
    * remove unused pattern
    
    * make apply transformations faster
    
    * remove file if exists
    
    * refactor using structs
    
    * rename bundler
    
    * rename some fns
    
    * make main cleaner
    
    * small tweaks
    
    * make some vars global
    
    * update readme
    
    * remove migrated script
    
    * removing file is not neccessary
    
    * put bundler into its own file
    
    * rename a var

---
+++ go-work/cmd/zsb_bundle/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"example.com/workspace/lib/utils"
+)
+
+var (
+	zsb    string
+	zsbDir = os.Getenv("HOME") + "/.zsh-spell-book"
+)
+
+func main() {
+	// Get zsb prefix from environment or default to 'zsb'
+	zsb = os.Getenv("zsb")
+	if zsb == "" {
+		zsb = "zsb"
+	}
+
+	bundler := NewBundler()
+
+	// Add dynamic prefix function
+	bundler.WriteString(fmt.Sprintf("%s.sourceFiles() for f in $*; do source $f; done\n", zsb))
+
+	bundler.BundleEnvFile()
+
+	bundler.BundleFile("src/zsh.config.zsh")
+	bundler.BundleFile("src/globalVariables.zsh")
+
+	bundler.BundleDir("src/utils")
+	bundler.BundleDir("src/configurations")
+	bundler.BundleDir("src/spells")
+	bundler.BundleDir("src/temp/spells")
+	bundler.BundleDir("src/automatic-calls")
+
+	result := bundler.ApplyTransformations()
+
+	resultPath := filepath.Join(zsbDir, "result.zsh")
+	utils.Must(os.WriteFile(resultPath, []byte(result), 0644))
+
+	fmt.Println("ℨ𝔰𝔥 𝔖𝔭𝔢𝔩𝔩𝔟𝔬𝔬𝔨 bundled!!")
+}
