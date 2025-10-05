4434b2ecd02535349f8d568c3789c56ab4504db9
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2025-10-05 20:44:50 -0300

    open files using nvim instead

--- go-work/cmd/airbnb_calculator/main.go
+++ go-work/cmd/airbnb_calculator/main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os/exec"
 
 	"example.com/workspace/lib/args"
-	"example.com/workspace/lib/open"
 	u "example.com/workspace/lib/utils"
 )
+
+// openFilesInTmux creates a new tmux tab with neovim in horizontal split
+// filename at the top pane and outputFilename at the bottom pane
+func openFilesInTmux(filename, outputFilename string) error {
+	// Create a new tmux window with the first file
+	cmd := exec.Command("tmux", "new-window", "nvim", filename)
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to create tmux window: %v", err)
+	}
+
+	// Split the window horizontally and open the second file in the bottom pane
+	cmd = exec.Command("tmux", "split-window", "-v", "nvim", outputFilename)
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to split tmux window: %v", err)
+	}
+
+	return nil
+}
 
 func main() {
 	d := u.Must1(args.Parse())
@@ -31,8 +49,7 @@ func main() {
 
 	u.Must(writeToCsv(records[:], outputFilename))
 
-	open.File(filename)
-	open.File(outputFilename)
+	u.Must(openFilesInTmux(filename, outputFilename))
 
-	fmt.Printf("Opening files '%s' and '%s'.\n", filename, outputFilename)
+	fmt.Printf("Opening files '%s' and '%s' in tmux with neovim.\n", filename, outputFilename)
 }
