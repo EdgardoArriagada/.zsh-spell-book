fdee6510c1b06b75f0dc917e178ae5fb2a24b054
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2026-03-24 13:11:29 -0300

    improve hasRust

--- src/spells/rust/hasRust.zsh
+++ src/spells/rust/hasRust.zsh
@@ -1,7 +1,12 @@
 hasRust() {
-  <<< ''; printAndRun "rustc --version"
-  <<< ''; printAndRun "cargo --version"
-  <<< ''; printAndRun "rustup --version"
-  <<< ''
-  ${zsb}.info "run `hl 'rustup update stable'` to update to the latest stable release"
+  if ! printAndRun "rustc --version"; then
+    ${zsb}.info "Rust not found. Redirecting to `hl 'https://rustup.rs'`"
+    loading 2
+    zsb_open "https://rustup.rs"
+    return 1
+  fi
+
+  printAndRun "cargo --version"
+  printAndRun "rustup --version"
+  ${zsb}.info "run `hl 'rustup update'` to update all toolchains and rustup itself"
 }
