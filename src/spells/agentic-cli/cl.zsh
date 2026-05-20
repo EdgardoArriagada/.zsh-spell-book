459aa4cf42051891b1655e0994cce8036d4257e4
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2026-05-20 15:44:16 -0400

    agentic cli

---
+++ src/spells/agentic-cli/cl.zsh
@@ -0,0 +1,4 @@
+alias cl='claude --model=opus --dangerously-skip-permissions'
+alias clp='cl --permission-mode plan'
+
+hisIgnore cl clp
--- src/spells/claude/cl.zsh
+++
@@ -1,4 +0,0 @@
-alias cl='claude --dangerously-skip-permissions'
-alias clp='cl --permission-mode plan'
-
-hisIgnore cl clp
