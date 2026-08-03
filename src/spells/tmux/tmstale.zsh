db7c5c4f8ecb8a4a52e51fceb987ccc7cb5166db
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2026-08-03 16:39:00 -0400

    tmstale

---
+++ src/spells/tmux/tmstale.zsh
@@ -0,0 +1,11 @@
+tmstale() {
+  local session=$(tmdiff | fzf --exit-0)
+  [[ -z "$session" ]] && ${zsb}.cancel 'There are no stale sessions.'
+
+  if [[ -n "$TMUX" ]]
+    then tmux switch-client -t "=$session"
+    else tmux attach-session -t "=$session"
+  fi
+}
+
+hisIgnore tmstale
