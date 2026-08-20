ada2ac73bb9cd64aa47e319e0127c6a9bbdb2038
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2026-08-20 10:52:32 -0400

    erase unnecessary sund

--- src/spells/tmux/agent-notif.zsh
+++ src/spells/tmux/agent-notif.zsh
@@ -1,10 +1,5 @@
-mf() {
-  zsb_tmux_agent_notification --finished _ $TMUX_PANE;
-  zsb_play ~/.zsh-spell-book/src/media/sounds/aoe_farm.wav
-}
+mf() { zsb_tmux_agent_notification --finished _ $TMUX_PANE; }
 
-mw() {
-  zsb_tmux_agent_notification --working _ $TMUX_PANE;
-}
+mw() { zsb_tmux_agent_notification --working _ $TMUX_PANE; }
 
 hisIgnore mf mw
