f4cab6af96aa62b998aff2f86c8d11bb8312b7c9
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2026-09-02 23:08:33 -0400

    ff instead

--- src/spells/tmux/agent-notif.zsh
+++ src/spells/tmux/agent-notif.zsh
@@ -1,4 +1,4 @@
-mf() { zsb_tmux_agent_notification --finished _ $TMUX_PANE; }
+mf() { zsb_tmux_agent_notification --force-finished _ $TMUX_PANE; }
 
 mw() { zsb_tmux_agent_notification --working _ $TMUX_PANE; }
 
